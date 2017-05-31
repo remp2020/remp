083a7ece4c9a55f5045c910820e59a1fc2818b4b
Peter Dulacka <dulacka@gmail.com>
2017-05-31 14:58:17 +0200

    Removing testing and obsolete JS snippet for preview.
    remp/remp#27

--- Campaign/resources/views/welcome.blade.php
+++ Campaign/resources/views/welcome.blade.php
@@ -1,20 +1,4 @@
 @extends('layouts.app')
 
-@push('scripts')
-<script type="text/javascript">
-    var rempCampaign = {
-        "userId": "92363"
-    };
-    (function () {
-        var s = document.createElement('script');
-        s.type = 'text/javascript';
-        s.async = true;
-        s.src = 'http://rempcampaign.local/assets/js/lib.js';
-        var p = document.getElementsByTagName('script')[0];
-        p.parentNode.insertBefore(s, p);
-    })();
-</script>
-@endpush
-
 @section('content')
 @endsection
