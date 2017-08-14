5056b994f0f045e64888a37d6b12baaeb6824741
Davidko Beran <davidko.beran@gmail.com>
2017-08-14 10:04:49 +0200

    added form to create newsletter list
    remp/remp#36

---
+++ Mailer/app/forms/ListFormFactory.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Remp\MailerModule\Forms;
+
+use Nette\Application\UI\Form;
+use Nette\Object;
+use Remp\MailerModule\Repository\ListCategoriesRepository;
+use Remp\MailerModule\Repository\ListsRepository;
+
+class ListFormFactory extends Object
+{
+    /** @var ListsRepository */
+    private $listsRepository;
+
+    /** @var ListCategoriesRepository */
+    private $listCategoriesRepository;
+
+    public $onSuccess;
+
+    public function __construct(
+        ListsRepository $listsRepository,
+        ListCategoriesRepository $listCategoriesRepository
+    ) {
+        $this->listsRepository = $listsRepository;
+        $this->listCategoriesRepository = $listCategoriesRepository;
+    }
+
+    public function create()
+    {
+        $form = new Form;
+        $form->addProtection();
+
+        $categoryPairs = $this->listCategoriesRepository->all()->fetchPairs('id', 'title');
+        $form->addSelect('mail_type_category_id', 'Category', $categoryPairs);
+
+        $form->addSelect('priority', 'Priority', [10 => 'High', '100' => 'Normal', 1000 => 'Low']);
+
+        $form->addText('code', 'Code')
+            ->setRequired('Required');
+
+        $form->addText('title', 'Title')
+            ->setRequired('Required');
+
+        $form->addTextArea('description', 'Description')
+            ->setAttribute('rows', 3);
+
+        $form->addText('preview_url', 'Preview URL');
+
+        $form->addText('image_url', 'Image URL');
+
+        $order = ['begin' => 'At the beginning', 'end' => 'At the end', 'after' => 'After'];
+        $form->addRadioList('sorting', 'Order', $order);
+
+        $orderPairs = $this->listsRepository->findByCategory(key($categoryPairs))->fetchPairs('sorting', 'title');
+        $form->addSelect('sorting_after', null, $orderPairs);
+
+        $form->addCheckbox('auto_subscribe', 'Required user consent');
+        $form->addCheckbox('locked', 'Locked');
+        $form->addCheckbox('is_public', 'Public');
+
+        $form->addSubmit('save', 'Save')
+            ->getControlPrototype()
+            ->setName('button')
+            ->setHtml('<i class="zmdi zmdi-mail-send"></i> Save');
+
+        $form->onSuccess[] = [$this, 'formSucceeded'];
+        return $form;
+    }
+
+    public function formSucceeded($form, $values)
+    {
+        switch ($values['sorting']) {
+            case 'begin':
+                $values['sorting'] = 1;
+                break;
+
+            case 'after':
+                $values['sorting'] = $values['sorting_after'] + 1;
+                break;
+
+            default:
+            case 'end':
+                $values['sorting'] = $this->listsRepository->findByCategory($values['mail_type_category_id'])->count('*') + 1;
+                break;
+        }
+
+        $row = $this->listsRepository->add(
+            $values['mail_type_category_id'],
+            $values['priority'],
+            $values['code'],
+            $values['title'],
+            $values['sorting'],
+            $values['auto_subscribe'],
+            $values['locked'],
+            $values['is_public'],
+            $values['description'],
+            $values['preview_url'],
+            $values['image_url']
+        );
+        ($this->onSuccess)($row);
+    }
+}
