7fa24b4a2d4bbec75c7cafe85de5a11ed93af38b
Peter Dulacka <dulacka@gmail.com>
2017-08-23 18:20:02 +0200

    Adding migrations, fixing yarn install and Dockerfile ENVs.

--- Docker/php/remp.sh
+++ Docker/php/remp.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
+
 cd ${APP_NAME}
+
 if [ ! -f ".env" ]
 then
     cp .env.example .env
     composer install
-    yarn install
+    yarn install --no-bin-links
     yarn run dev
     if [ -f "artisan" ]
     then
+        php artisan migrate
         php artisan db:seed
     elif [ -f "bin/command.php" ]
     then
-        php bin/command.php application:seed
+        #php bin/command.php db:migrate
+        php bin/command.php db:seed
     fi
 fi
 php-fpm
--- Mailer/app/commands/DatabaseSeedCommand.php
+++ Mailer/app/commands/DatabaseSeedCommand.php
@@ -36,7 +36,7 @@ class DatabaseSeedCommand extends Command
 
     protected function configure()
     {
-        $this->setName('application:seed')
+        $this->setName('db:seed')
             ->setDescription('Seed database with required values');
     }
 
