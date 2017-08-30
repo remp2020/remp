e698531fdb297a52eb8834f6083526b3c02ebeee
Davidko Beran <davidko.beran@gmail.com>
2017-08-30 12:05:11 +0200

    added seeder for required configuration
    remp/remp#53

--- Mailer/app/commands/DatabaseSeedCommand.php
+++ Mailer/app/commands/DatabaseSeedCommand.php
@@ -2,36 +2,20 @@
 
 namespace Remp\MailerModule\Commands;
 
-use Nette\Database\Table\ActiveRow;
-use Remp\MailerModule\Repository\LayoutsRepository;
-use Remp\MailerModule\Repository\ListCategoriesRepository;
-use Remp\MailerModule\Repository\ListsRepository;
-use Remp\MailerModule\Repository\TemplatesRepository;
+use Remp\MailerModule\Repository\ConfigsRepository;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class DatabaseSeedCommand extends Command
 {
-    private $layoutsRepository;
-
-    private $templatesRepository;
-
-    private $listsRepository;
-
-    private $listCategoriesRepository;
+    private $configsRepository;
 
     public function __construct(
-        LayoutsRepository $layoutsRepository,
-        TemplatesRepository $templatesRepository,
-        ListsRepository $listsRepository,
-        ListCategoriesRepository $listCategoriesRepository
+        ConfigsRepository $configsRepository
     ) {
         parent::__construct();
-        $this->layoutsRepository = $layoutsRepository;
-        $this->templatesRepository = $templatesRepository;
-        $this->listsRepository = $listsRepository;
-        $this->listCategoriesRepository = $listCategoriesRepository;
+        $this->configsRepository = $configsRepository;
     }
 
     protected function configure()
@@ -51,97 +35,24 @@ class DatabaseSeedCommand extends Command
         $output->writeln('<info>***** APPLICATION SEEDER *****</info>');
         $output->writeln('');
 
-        // list category
-        $output->write('List categories: ');
-        /** @var ActiveRow $category */
-        $category = $this->listCategoriesRepository->findBy('title', 'DEMO newsletters');
-        if (!$category) {
-            $category = $this->listCategoriesRepository->insert([
-                'title' => 'DEMO newsletters',
-                'sorting' => 100,
-                'created_at' => new \DateTime(),
-            ]);
-        }
-        $output->writeln('<info>OK!</info>');
-
-        // list
-        $output->write('Lists: ');
-        /** @var ActiveRow $list */
-        $list = $this->listsRepository->findBy('code', 'demo-weekly-newsletter');
-        if (!$list) {
-            $list = $this->listsRepository->add(
-                $category->id,
-                100,
-                'demo-weekly-newsletter',
-                'DEMO Weekly newsletter',
-                100,
-                false,
-                false,
-                true,
-                'Example mail list'
-            );
-        }
-        $output->writeln('<info>OK!</info>');
-
-        // layout
-        $output->write('Layouts: ');
-        /** @var ActiveRow $layout */
-        $layout = $this->layoutsRepository->findBy('name', 'DEMO layout');
-        if (!$layout) {
-            $text = <<<EOF
-REMP - Reader's Engagement and Monetization Platform
-
-{{ content|raw }}
-
-&copy; REMP, {{ time|date('Y') }}
-EOF;
-            $html = <<<HTML
-<!DOCTYPE html>
-<html lang="en" dir="ltr" xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-    	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-		<title>REMP - Demo email</title>
-	</head>
-	<body style="-webkit-size-adjust: none; -ms-text-size-adjust: 100%; width: 400px; max-width: 400px;">
-	    <h2>REMP - Reader's Engagement and Monetization Platform</h2>
-        {{ content|raw }}
-        <p><em><small>&copy; REMP, {{ time|date('Y') }}</small></em></p>
-	</body>
-</html>
-HTML;
-            $layout = $this->layoutsRepository->add('DEMO layout', $text, $html);
-        }
-        $output->writeln('<info>OK!</info>');
-
-        // email
-        $output->write('Emails: ');
-        $email = $this->templatesRepository->findBy('code', 'demo-email');
-        if (!$email) {
-            $text = <<<EOF
-Text content of DEMO email.
-EOF;
-            $html = <<<HTML
-<table style="width: 400px; background-color: #eee">
-    <tr>
-        <td>Text content of <em>DEMO email</em>.</td>
-    </tr>
-</table>
-HTML;
-
-            $this->templatesRepository->add(
-                'DEMO email',
-                'demo-email',
-                'Demonstration-purpose email',
-                true,
-                'REMP <info@remp2020.com>',
-                'REMP - DEMO',
-                $text,
-                $html,
-                $layout->id,
-                $list->id
-            );
+        $output->writeln('Required configuration: ');
+        $configValues = [
+            ['default_mailer', 'Default Mailer', null, '', 'string'],
+        ];
+        foreach ($configValues as $configValue) {
+            $config = $this->configsRepository->findBy('name', $configValue['0']);
+            if (!$config) {
+                $config = $this->configsRepository->add(
+                    $configValue[0],
+                    $configValue[1],
+                    $configValue[2],
+                    $configValue[3],
+                    $configValue[4]
+                );
+                $output->writeln(" * Config <info>{$configValue['0']}</info> created");
+            } else {
+                $output->writeln(" * Config <info>{$configValue['0']}</info> exists");
+            }
         }
         $output->writeln('<info>OK!</info>');
     }
