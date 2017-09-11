bb47362ee72894cf4aa113708045965e9a1e3507
Peter Dulacka <dulacka@gmail.com>
2017-09-11 08:17:16 +0200

    Switch with custom input box for entering value.
    remp/remp#81

--- Beam/resources/assets/js/components/vSelect.vue
+++ Beam/resources/assets/js/components/vSelect.vue
@@ -1,13 +1,26 @@
 <template>
-    <select :data-type="dataType" :multiple="multiple" class="selectpicker" :data-live-search="livesearch" :disabled="disabled">
-        <option :value="option.value || option" v-for="option in options">
-            {{ option.label || option.value || option }}
-        </option>
-    </select>
+    <div>
+        <div class="row">
+            <div class="col-xs-10">
+                <select :name="name" :data-type="dataType" :multiple="multiple" class="selectpicker" :data-live-search="livesearch" :disabled="disabled">
+                    <option :value="option.value || option" v-for="option in options">
+                        {{ option.label || option.value || option }}
+                    </option>
+                </select>
+                <input v-show="customInput" v-model="customValue" :name="name" placeholder="e.g. my-event" title="Custom value" type="text" required="required" class="form-control fg-input">
+            </div>
+            <div class="col-xs-2">
+                <span v-on:click="customInput = !customInput" :class="[{'palette-Blue-Grey bg': customInput}, {'btn-default': !customInput}, 'btn', 'waves-effect']">
+                    <i class="zmdi zmdi-hc-lg zmdi-edit"></i>
+                </span>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
     let props = [
+        'name',
         'options',
         'value',
         'multiple',
@@ -19,10 +32,21 @@
     export default {
         name: "v-select",
         props: props,
+        data: function() {
+            return {
+                customInput: false,
+                customValue: null,
+            }
+        },
         mounted: function () {
             let vm = this;
-            $(this.$el).selectpicker('val', this.value !== null ? this.value : null);
-            $(this.$el).on('changed.bs.select', function () {
+            let $select = $(this.$el).find('select');
+            if (this.value !== null) {
+                $select.selectpicker('val', this.value !== null ? this.value : null);
+                this.customValue = this.value;
+            }
+
+            $select.on('changed.bs.select', function () {
                 let val = $(this).val();
                 vm.$parent.$emit("select-changed", {
                     type: vm.dataType,
@@ -41,6 +65,15 @@
         },
         destroyed : function () {
             $(this.$el).selectpicker('destroy');
+        },
+        watch: {
+            customInput: function(val) {
+                if (val) {
+                    $(this.$el).find('.selectpicker').selectpicker('hide');
+                } else {
+                    $(this.$el).find('.selectpicker').selectpicker('show');
+                }
+            },
         },
     }
 </script>
