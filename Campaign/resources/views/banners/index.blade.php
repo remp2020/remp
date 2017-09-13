6dad23fb18836fe54d26b1950d7e24e027bdd4c3
Peter Dulacka <dulacka@gmail.com>
2017-09-13 13:43:35 +0200

    Adding modal window with template selection into banner creation step.

--- Campaign/resources/views/banners/index.blade.php
+++ Campaign/resources/views/banners/index.blade.php
@@ -11,7 +11,7 @@
         <div class="card-header">
             <h2>List of banners <small></small></h2>
             <div class="actions">
-                <a href="{{ route('banners.create') }}" class="btn palette-Cyan bg waves-effect">Add new banner</a>
+                <a href="#modal-template-select" data-toggle="modal" class="btn palette-Cyan bg waves-effect">Add new banner</a>
             </div>
         </div>
 
@@ -24,6 +24,43 @@
             ],
             'rowActionLink' => 'show',
         ]) !!}
+    </div>
+
+    <div class="modal" id="modal-template-select" data-keyboard="false" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Select template</h4>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h4 class="text-center">Custom HTML</h4>
+                                </div>
+                                <div class="card-body card-padding">
+                                    <div class="preview" style="margin-top: 55px;">
+                                        <i class="zmdi zmdi-language-html5 zmdi-hc-5x"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h4 class="text-center">Medium rectangle</h4>
+                                </div>
+                                <div class="card-body">
+                                    <div class="preview medium-rectangle"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
 @endsection
