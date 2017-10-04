da70731bb570fcfc3dbbdd6e15f445024c3076ab
Peter Dulacka <dulacka@gmail.com>
2017-10-04 13:30:24 +0200

    Prefixing banner's CSS to not touch main website.

--- Campaign/resources/assets/js/components/BannerPreview.vue
+++ Campaign/resources/assets/js/components/BannerPreview.vue
@@ -3,7 +3,7 @@
 </style>
 
 <template>
-    <div>
+    <div class="remp-banner">
         <html-preview v-if="template == 'html'"
                 :alignmentOptions="alignmentOptions"
                 :dimensionOptions="dimensionOptions"
