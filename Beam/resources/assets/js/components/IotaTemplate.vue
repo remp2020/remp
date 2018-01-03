0fa0507b88f8074db85c3b89c6a2974e8ca46451
Peter Dulacka <dulacka@gmail.com>
2018-01-03 11:12:47 +0100

    Iota template redesign.
    remp/remp#104

--- Beam/resources/assets/js/components/IotaTemplate.vue
+++ Beam/resources/assets/js/components/IotaTemplate.vue
@@ -1,32 +1,37 @@
 <style type="text/css">
-    .remp-iota-template {
-        background-color: #efefef;
-        color: black;
-        padding: 10px;
-        margin:5px 0 10px;
-    }
-    .remp-iota-template-counts {
-        display: inline-block;
-    }
+    .ri_box, .ri_box * { display: inline-block; margin: 0; padding: 0; border: 0; outline: 0; }
+    .ri_box { margin: 8px 0 }
+    .ri_box_line, .ri_box_logo svg { display: block; }
+    .ri_box { display: block; position: relative; z-index: 999999; line-height: 16px; font-size: 12px; font-weight: normal; background: #ffffff; color: #333333; }
+    .ri_box_line { padding: 2px 4px; clear: both; background: #eeeeee; }
+    .ri_box_line:nth-child(even) { background: transparent; }
+    .ri_box_small, .ri_box_key { font-size: 10px; font-weight: bold; color: #666666; }
+    .ri_box_title { font-weight: bold; color: #000000; }
+    .ri_box_logo { vertical-align: top; }
+    .ri_box_logo svg { width: 16px; height: 16px; }
+    .ri_box *[title] { border-bottom: 1px #cccccc dotted; }
+    .ri_box_green { color: #009900; }
+    .ri_box_red { color: #990000; }
+    .ri_box_blue { color: #000099; }
+    .ri_box_blue { color: #000099; }
+    .ri_box__collapsed .ri_box__collapsed_show, .ri_box__expanded .ri_box__expanded_show { display: block; }
+    .ri_box__collapsed .ri_box__collapsed_hide, .ri_box__expanded .ri_box__expanded_hide  { display: none; }
 </style>
 
 <template>
-    <div class="remp-iota-template">
-        <div v-if="sortedPageviewRanges.length > 0" class="remp-iota-template-category">
-            <div class="remp-iota-template-category-title">REMP pageview stats:</div>
-            <div class="remp-iota-template-category-stats">
-                <div v-for="range in sortedPageviewRanges">
-                    {{ range }}h: {{ pageviewStats[range].toFixed(2) }}
+    <div>
+        <div class="ri_box ri_box__expanded">
+            <div v-if="sortedRevenueRanges.length > 0" class="ri_box_line">
+                <div class="ri_box_logo">
+                    <svg viewBox="0 0 71.61 70.88"><polygon fill="black" points="35.8 2.74 16.91 13.65 23.2 17.29 35.8 10.02 54.7 20.93 35.8 31.84 35.8 31.84 10.61 17.29 10.61 53.59 16.91 57.23 16.91 28.2 35.8 39.11 35.8 39.11 35.8 39.11 35.8 39.11 61 24.57 61 17.29 35.8 2.74" /><polygon fill="black" points="23.2 53.66 23.2 60.93 35.8 68.14 61 53.59 61 46.32 35.8 60.86 23.2 53.66"/><polygon fill="black" points="35.8 46.28 23.2 39.08 23.2 46.35 35.8 53.55 35.8 53.66 61 39.11 61 31.84 35.8 46.39 35.8 46.28"/></svg>
                 </div>
-            </div>
-        </div>
-
-        <div v-if="sortedRevenueRanges.length > 0" class="remp-iota-template-category">
-            <div class="remp-iota-template-category-title">REMP revenue stats:</div>
-            <div class="remp-iota-template-category-stats">
-                <div v-for="range in sortedRevenueRanges">
-                    {{ range }}h: {{ revenueStats[range].toFixed(2) }} &euro;
-                    <div class="remp-iota-template-counts" v-if="countStats[range]">({{ countStats[range] }})</div>
+                <div class="ri_box_title">Revenue</div>
+                <div v-for="range in sortedRevenueRanges" class="ri_box_item">
+                    <div class="ri_box_key">{{ range }}H</div>
+                    <div class="ri_box_value">
+                        {{ revenueStats[range].toFixed(2) }}&euro;
+                        <span v-if="countStats[range]" class="ri_box_small">({{ countStats[range] }})</span>&nbsp; 
+                    </div>
                 </div>
             </div>
         </div>
@@ -34,7 +39,6 @@
 </template>
 
 <script type="text/javascript">
-    import Axios from "axios"
     import EventHub from "./EventHub"
 
     export default {
