26386fb695fa1854ef8753c0095d5f7ad006046d
Dominik Skerhak <dominik.skerhak@gmail.com>
2018-03-05 10:15:47 +0100

    Add MaxMind attribution requirement (CC BY-SA 4.0)
    README updated with text based on MaxMind License section
    (https://dev.maxmind.com/geoip/geoip2/geolite2/).
    
    MaxMind database doesn't need to be public & downloadable, leaving it in
    resources without copy in `public` dir.
    
    remp/remp#121

--- Campaign/app/Http/Controllers/CampaignController.php
+++ Campaign/app/Http/Controllers/CampaignController.php
@@ -361,7 +361,7 @@ class CampaignController extends Controller
             if (!empty($campaign->countries)) {
                 // load country ISO code based on IP
                 try {
-                    $reader = new GeoIp2\Database\Reader('./assets/vendor/maxmind/GeoLite2-Country.mmdb');
+                    $reader = new GeoIp2\Database\Reader(base_path() . '/resources/assets/maxmind/GeoLite2-Country.mmdb');
                     $record = $reader->country($_SERVER['REMOTE_ADDR']);
                     $countryCode = $record->country->isoCode;
                 } catch (\MaxMind\Db\Reader\InvalidDatabaseException | GeoIp2\Exception\AddressNotFoundException $e) {
