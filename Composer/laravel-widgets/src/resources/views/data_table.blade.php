ed8ff6c8b514f52f73640451d77623cb123bc3a6
Peter Dulacka <dulacka@gmail.com>
2018-03-23 15:44:34 +0100

    Bugfixing events being attached to all displayed grids, did split by ID
    remp/remp#202

--- Composer/laravel-widgets/src/resources/views/data_table.blade.php
+++ Composer/laravel-widgets/src/resources/views/data_table.blade.php
@@ -5,7 +5,7 @@
             <i class="ah-search-close zmdi zmdi-long-arrow-left" data-ma-action="ah-search-close"></i>
         </div>
 
-        <ul class="ah-actions actions a-alt">
+        <ul id="dt-nav-{{ $tableId }}" class="ah-actions actions a-alt">
             <li><button class="btn palette-Cyan bg ah-search-trigger" data-ma-action="ah-search-open"><i class="zmdi zmdi-search"></i></button></li>
             <li class="ah-length dropdown">
                 <button class="btn palette-Cyan bg" data-toggle="dropdown">10</button>
@@ -64,7 +64,10 @@
                     @if (isset($col['orderable']))
                     orderable: false,
                     @endif
-                            @if (isset($col['render']))
+                    @if (isset($col['searchable']))
+                    searchable: false,
+                    @endif
+                    @if (isset($col['render']))
                     render: $.fn.dataTables.render['{!! $col['render'] !!}']({!! isset($col['renderParams']) ? json_encode($col['renderParams']) : '' !!})
                     @endif
                 },
@@ -113,7 +116,7 @@
                 }
             },
             'drawCallback': function(settings) {
-                $.fn.dataTables.pagination(settings);
+                $.fn.dataTables.pagination(settings, 'dt-nav-{{ $tableId }}');
             },
             'initComplete': function (a,b,c,d) {
                 var state = this.api().state().columns;
@@ -130,7 +133,7 @@
             }
         });
 
-        $.fn.dataTables.navigation(dataTable);
+        $.fn.dataTables.navigation(dataTable, 'dt-nav-{{ $tableId }}');
 
         @foreach ($refreshTriggers as $def)
         $('{!! $def['selector'] !!}').on('{!! $def['event'] !!}', function() {
