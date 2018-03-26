3cc937598fb709df6f0e66f9ff2006c3240769ff
Peter Dulacka <dulacka@gmail.com>
2018-03-26 14:08:09 +0200

    Adding image and lock variant stats to IOTA service and template

--- Beam/resources/assets/js/components/IotaService.vue
+++ Beam/resources/assets/js/components/IotaService.vue
@@ -50,7 +50,7 @@
             let now = new Date();
             this.fetchCommerceStats(now);
             this.fetchPageviewStats(now);
-            this.fetchTitleVariantStats(now);
+            this.fetchVariantStats(now, ["title_variant", "image_variant", "lock_variant"]);
         },
         methods: {
             fetchCommerceStats: function(now) {
@@ -69,7 +69,7 @@
                         "group_by": [
                             "article_id",
                         ],
-                    }
+                    };
 
                     Axios.post(this.baseUrl + '/journal/commerce/steps/purchase/sum', payload)
                         .then(function (response) {
@@ -113,7 +113,7 @@
                         "group_by": [
                             "article_id",
                         ],
-                    }
+                    };
 
                     Axios.post(this.baseUrl + '/journal/pageviews/actions/load/count', payload)
                         .then(function (response) {
@@ -129,12 +129,12 @@
                 }
             },
 
-            fetchTitleVariantStats: function(now) {
+            fetchVariantStats: function(now, variantTypes) {
                 for (let range of this.pageviewMinuteRanges) {
                     let d = new Date(now.getTime());
                     d.setMinutes(d.getMinutes() - range.minutes);
 
-                    const titleVariantPayload = {
+                    const variantPayload = {
                         "time_after": d.toISOString(),
                         "filter_by": [
                             {
@@ -145,13 +145,12 @@
                         "group_by": [
                             "article_id",
                             "social",
-                            "title_variant",
-                        ],
+                        ].concat(variantTypes),
                     };
 
-                    Axios.post(this.baseUrl + '/journal/pageviews/actions/load/count', titleVariantPayload)
+                    Axios.post(this.baseUrl + '/journal/pageviews/actions/load/count', variantPayload)
                         .then(function (response) {
-                            let counts = {}
+                            let counts = {};
                             for (const group of response.data) {
                                 if (group["tags"]["social"] !== "") {
                                     // social networks always get variant A due to the nature of replacing
@@ -162,15 +161,26 @@
                                     continue;
                                 }
 
-                                const titleVariant = group["tags"]["title_variant"]
-                                const articleId = group["tags"]["article_id"]
-                                if (!counts[articleId]) {
-                                    counts[articleId] = {};
+                                for (const variantType of variantTypes) {
+                                    const variant = group["tags"][variantType];
+                                    if (variant === "") {
+                                        continue;
+                                    }
+                                    const articleId = group["tags"]["article_id"];
+                                    if (!counts[variantType]) {
+                                        counts[variantType] = {};
+                                    }
+                                    if (!counts[variantType][articleId]) {
+                                        counts[variantType][articleId] = {}
+                                    }
+                                    if (!counts[variantType][articleId][variant]) {
+                                        counts[variantType][articleId][variant] = 0;
+                                    }
+                                    counts[variantType][articleId][variant] += group["count"]
                                 }
-                                counts[articleId][titleVariant] = group["count"]
                             }
-                            EventHub.$emit('content-title-variants-changed', range, counts)
 
+                            EventHub.$emit('content-variants-changed', variantTypes, range, counts)
                         })
                         .catch(function (error) {
                             console.warn(error);
