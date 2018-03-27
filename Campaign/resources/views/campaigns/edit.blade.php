00c8ea1f6fa160b563fbb7035fb484727726f26d
Adam Žoldák <zoldakadam@gmail.com>
2018-03-27 13:38:33 +0200

    redesign campaign form
    remp/remp#207

--- Campaign/resources/views/campaigns/edit.blade.php
+++ Campaign/resources/views/campaigns/edit.blade.php
@@ -4,47 +4,44 @@
 
 @section('content')
 
-    <div class="c-header">
-        <h2>Campaigns: {{ $campaign->name }}</h2>
-    </div>
-    <div class="card">
-        <div class="card-header">
-            <h2>Edit campaign <small>{{ $campaign->name }}</small></h2>
-        </div>
+    <div class="container">
+        @include('flash::message')
 
-        <div class="card-body card-padding">
-            @include('flash::message')
-
-            {!! Form::model($campaign, ['route' => ['campaigns.update', $campaign], 'method' => 'PATCH']) !!}
-            @include('campaigns._form')
-            {!! Form::close() !!}
-        </div>
+        {!! Form::model($campaign, ['route' => ['campaigns.update', $campaign], 'method' => 'PATCH']) !!}
+        @include('campaigns._form')
+        {!! Form::close() !!}
     </div>
 
-    <div class="card">
-        <div class="card-header">
-            <h2>Scheduled runs<small></small></h2>
-        </div>
-        <div class="card-body">
-            {!! Widget::run('DataTable', [
-            'colSettings' => [
-                'campaign' => [
-                    'header' => 'Campaign',
-                ],
-                'start_time' => [
-                    'header' => 'Scheduled start date',
-                    'render' => 'date',
-                ],
-                'end_time' => [
-                    'header' => 'Scheduled end date',
-                    'render' => 'date',
-                ],
-                'status' => [
-                    'header' => 'Status',
-                ],
-            ],
-            'dataSource' => route('campaign.schedule.json', ['campaign' => $campaign]),
-            ]) !!}
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-lg-8">
+                <div class="card z-depth-1-top">
+                    <div class="card-header">
+                        <h2>Scheduled runs<small></small></h2>
+                    </div>
+                    <div class="card-body">
+                        {!! Widget::run('DataTable', [
+                        'colSettings' => [
+                            'campaign' => [
+                                'header' => 'Campaign',
+                            ],
+                            'start_time' => [
+                                'header' => 'Scheduled start date',
+                                'render' => 'date',
+                            ],
+                            'end_time' => [
+                                'header' => 'Scheduled end date',
+                                'render' => 'date',
+                            ],
+                            'status' => [
+                                'header' => 'Status',
+                            ],
+                        ],
+                        'dataSource' => route('campaign.schedule.json', ['campaign' => $campaign]),
+                        ]) !!}
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
