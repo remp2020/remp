de2f32d31d29377d4637d6be06cba8fd6aebdb05
Miroslav Svítok <mirosvitok@gmail.com>
2018-05-04 09:20:49 +0200

    Mailer - generator Preprocessing API takes JSON, returns also generator URL

--- Mailer/app/api/v1/Handlers/Mailers/MailGeneratorPreprocessHandler.php
+++ Mailer/app/api/v1/Handlers/Mailers/MailGeneratorPreprocessHandler.php
@@ -2,10 +2,10 @@
 
 namespace Remp\MailerModule\Api\v1\Handlers\Mailers;
 
+use Nette\Application\LinkGenerator;
 use Remp\MailerModule\Generators\GeneratorFactory;
 use Remp\MailerModule\Repository\SourceTemplatesRepository;
 use Tomaj\NetteApi\Handlers\BaseHandler;
-use Tomaj\NetteApi\Params\InputParam;
 use Tomaj\NetteApi\Response\JsonApiResponse;
 
 class MailGeneratorPreprocessHandler extends BaseHandler
@@ -14,25 +14,33 @@ class MailGeneratorPreprocessHandler extends BaseHandler
 
     private $sourceTemplatesRepository;
 
-    public function __construct(GeneratorFactory $generatorFactory, SourceTemplatesRepository $sourceTemplatesRepository)
+    public function __construct(
+        LinkGenerator $linkGenerator,
+        GeneratorFactory $generatorFactory,
+        SourceTemplatesRepository $sourceTemplatesRepository)
     {
         parent::__construct();
         $this->generatorFactory = $generatorFactory;
         $this->sourceTemplatesRepository = $sourceTemplatesRepository;
-    }
-
-    public function params()
-    {
-        return [
-            new InputParam(InputParam::TYPE_POST, 'source_template_id', InputParam::REQUIRED),
-            new InputParam(InputParam::TYPE_POST, 'data', InputParam::REQUIRED),
-        ];
+        $this->linkGenerator = $linkGenerator;
     }
 
     public function handle($params)
     {
+        $json = file_get_contents("php://input");
+        if (empty($json)) {
+            $response = new JsonApiResponse(400, ['status' => 'error', 'message' => 'Empty request']);
+            return $response;
+        }
+
+        $data = json_decode($json);
+
+        if (!isset($data->source_template_id)){
+            return new JsonApiResponse(400, ['status' => 'error', 'message' => 'Missing required json parameter \'source_template_id\'']);
+        }
+
         $generator = null;
-        $template = $this->sourceTemplatesRepository->find($params['source_template_id']);
+        $template = $this->sourceTemplatesRepository->find($data->source_template_id);
         if (!$template) {
             return new JsonApiResponse(400, ['status' => 'error', 'message' => "No source template associated with id {$params['source_template_id']}"]);
         }
@@ -43,9 +51,13 @@ class MailGeneratorPreprocessHandler extends BaseHandler
             return new JsonApiResponse(400, ['status' => 'error', 'message' => "Unregistered generator type {$template->generator}"]);
         }
 
-        $output = $generator->preprocessParameters(json_decode($params['data']));
+        $output = $generator->preprocessParameters($data->data);
+        $output->source_template_id = $data->source_template_id;
 
-
-        return new JsonApiResponse(200, ['status' => 'ok', 'data' => $output, 'generator_post_url'=>'http://something']);
+        return new JsonApiResponse(200, [
+            'status' => 'ok',
+            'data' => $output,
+            'generator_post_url' => $this->linkGenerator->link('Mailer:MailGenerator:default')
+        ]);
     }
 }
