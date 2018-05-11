e99fd271a097b527cb359919075d23d18f75d21d
Miroslav Svítok <mirosvitok@gmail.com>
2018-05-11 12:52:00 +0200

    Removed redundant constructor

--- Mailer/app/models/Generators/PreprocessException.php
+++ Mailer/app/models/Generators/PreprocessException.php
@@ -4,8 +4,4 @@ namespace Remp\MailerModule\Api\v1\Handlers\Mailers;
 
 class PreprocessException extends \Exception
 {
-    public function __construct($message)
-    {
-        parent::__construct($message);
-    }
 }
