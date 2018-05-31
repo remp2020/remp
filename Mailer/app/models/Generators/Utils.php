d16753835c903bc3c8275c5962d86dc6e28a35eb
Miroslav Svítok <mirosvitok@gmail.com>
2018-05-31 16:00:32 +0200

    Generic best performing articles generator

--- Mailer/app/models/Generators/Utils.php
+++ Mailer/app/models/Generators/Utils.php
@@ -7,14 +7,19 @@ use Remp\MailerModule\Api\v1\Handlers\Mailers\InvalidUrlException;
 use Remp\MailerModule\PageMeta\ContentInterface;
 use Remp\MailerModule\PageMeta\GuzzleTransport;
 use Remp\MailerModule\PageMeta\PageMeta;
+use Remp\MailerModule\PageMeta\TransportInterface;
 
 class Utils
 {
-    public static function fetchUrlMeta($url, ContentInterface $content)
+    public static function fetchUrlMeta($url, ContentInterface $content, TransportInterface $transportInterface = null)
     {
+        if ($transportInterface == null) {
+            $transportInterface = new GuzzleTransport();
+        }
+
         $url = preg_replace('/\\?ref=(.*)/', '', $url);
         try {
-            $pageMeta = new PageMeta(new GuzzleTransport(), $content);
+            $pageMeta = new PageMeta($transportInterface, $content);
             $meta = $pageMeta->getPageMeta($url);
             if ($meta) {
                 return $meta;
