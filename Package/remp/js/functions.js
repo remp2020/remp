344cc1fefb8b6ab202a0d56335cdbbc9987e0caa
Miroslav Svítok <mirosvitok@gmail.com>
2018-06-12 14:23:10 +0200

    Do not attach wave animation automatically

--- Package/remp/js/functions.js
+++ Package/remp/js/functions.js
@@ -341,8 +341,6 @@ $(document).ready(function(){
      * Waves Animation
      */
     (function(){
-        Waves.attach('.btn:not(.btn-icon):not(.btn-float)');
-        Waves.attach('.btn-icon, .btn-float', ['waves-circle', 'waves-float']);
         Waves.init();
     })();
 
