a4849d6ffef5a1fdc36064391b6f2421ee0b4d1c
Miroslav Svítok <mirosvitok@gmail.com>
2018-06-19 14:23:38 +0200

    Beam success notifications are shown now

--- Beam/resources/views/layouts/app.blade.php
+++ Beam/resources/views/layouts/app.blade.php
@@ -229,6 +229,30 @@
         })(delay);
         delay += 250;
         @endforeach
+        @if (session('warning'))
+        $.notify({
+            message: '{{ session('warning') }}'
+        }, {
+            allow_dismiss: false,
+            type: 'warning',
+            placement: {
+                from: "bottom",
+                align: "left"
+            }
+        });
+        @endif
+        @if (session('success'))
+        $.notify({
+            message: '{{ session('success') }}'
+        }, {
+            allow_dismiss: false,
+            type: 'info',
+            placement: {
+                from: "bottom",
+                align: "left"
+            }
+        });
+        @endif
     });
 </script>
 
