15646aec2b39413d08c015c2160e628f77dbd489
Miroslav Svítok <mirosvitok@gmail.com>
2018-07-30 10:51:17 +0200

    mail_source_template migration - make code unique and not nullable

--- Mailer/migrations/20180726110436_add_code_to_mail_source_template.php
+++ Mailer/migrations/20180726110436_add_code_to_mail_source_template.php
@@ -1,5 +1,6 @@
 <?php
 
+use Nette\Utils\Strings;
 use Phinx\Migration\AbstractMigration;
 
 class AddCodeToMailSourceTemplate extends AbstractMigration
@@ -8,6 +9,15 @@ class AddCodeToMailSourceTemplate extends AbstractMigration
     {
         $this->table('mail_source_template')
             ->addColumn('code', 'string', ['null' => true, 'after' => 'title'])
+            ->addIndex(['code'], ['unique' => true])
             ->update();
+
+        foreach ($this->fetchAll('select * from mail_source_template') as $row) {
+            $code = Strings::webalize($row['title']);
+            $this->execute("UPDATE mail_source_template SET code='$code' WHERE id={$row['id']}");
+        }
+
+        $this->table('mail_source_template')
+            ->changeColumn('code', 'string', ['null' => false]);
     }
 }
