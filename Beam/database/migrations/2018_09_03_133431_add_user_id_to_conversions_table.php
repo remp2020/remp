8860eff5933cebc4ce23294e43f81c050d37b26b
Miroslav Svítok <mirosvitok@gmail.com>
2018-09-03 15:48:12 +0200

    Add user ID to conversions table

---
+++ Beam/database/migrations/2018_09_03_133431_add_user_id_to_conversions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserIdToConversionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('conversions', function (Blueprint $table) {
+            $table->string('user_id')->after('article_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('conversions', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+        });
+    }
+}
