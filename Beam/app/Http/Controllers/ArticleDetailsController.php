048ae67f9a44f7695b180a91393686a6b25b04c5
Miroslav Svítok <mirosvitok@gmail.com>
2018-09-05 12:35:12 +0200

    Show subscription information in Article Details page

--- Beam/app/Http/Controllers/ArticleDetailsController.php
+++ Beam/app/Http/Controllers/ArticleDetailsController.php
@@ -3,16 +3,68 @@
 namespace App\Http\Controllers;
 
 use App\Article;
+use App\Contracts\JournalAggregateRequest;
+use App\Contracts\JournalContract;
 use App\Http\Resources\ArticleResource;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ArticleDetailsController extends Controller
 {
+    private $journal;
+
+    public function __construct(JournalContract $journal)
+    {
+
+        $this->journal = $journal;
+    }
+
     public function show(Article $article, Request $request)
     {
+        $timeBefore = Carbon::now();
+        $timeAfter = $article->published_at;
+
+        $uniqueRequest = new JournalAggregateRequest('pageviews', 'browsers');
+        $uniqueRequest->setTimeAfter($timeAfter);
+        $uniqueRequest->setTimeBefore($timeBefore);
+        $uniqueRequest->addGroup('article_id');
+        $uniqueRequest->addFilter('article_id', $article->external_id);
+        $results = $this->journal->unique($uniqueRequest);
+        $uniqueBrowsersCount = $results[0]->count;
+
+        $conversionRate = ($article->conversions()->count() / $uniqueBrowsersCount) * 100;
+
+        $newSubscriptionsCountSql = <<<SQL
+        select count(*) as subscriptions_count from (
+            select c1.* from conversions c1
+            left join conversions c2
+            on c1.user_id = c2.user_id and c2.paid_at < c1.paid_at
+            where c2.id is Null
+            and c1.article_id = ?
+        ) t
+SQL;
+        $newSubscriptionsCount = DB::select($newSubscriptionsCountSql, [$article->id])[0]->subscriptions_count;
+
+        $renewSubscriptionsCountSql = <<<SQL
+        select count(*) as subscriptions_count from (
+            select c1.user_id from conversions c1
+            left join conversions c2
+            on c1.user_id = c2.user_id and c2.paid_at < c1.paid_at and c2.id != c1.id
+            where c2.id is not Null
+            and c1.article_id = ?
+            group by user_id
+        ) t
+SQL;
+        $renewSubscriptionsCount = DB::select($renewSubscriptionsCountSql, [$article->id])[0]->subscriptions_count;
+
         return response()->format([
             'html' => view('articles.show', [
                 'article' => $article,
+                'conversionRate' => $conversionRate,
+                'uniqueBrowsersCount' => $uniqueBrowsersCount,
+                'newSubscriptionsCount' => $newSubscriptionsCount,
+                'renewSubscriptionsCount' => $renewSubscriptionsCount,
                 'dataFrom' => $request->input('data_from', 'now - 30 days'),
                 'dataTo' => $request->input('data_to', 'now'),
             ]),
--- Beam/resources/views/articles/show.blade.php
+++ Beam/resources/views/articles/show.blade.php
@@ -5,7 +5,102 @@
 @section('content')
 
     <div class="c-header">
-        <h2>{{ $article->title }}</h2>
+        <h2>Article Details</h2>
+    </div>
+
+
+    <div class="card" id="profile-main">
+        <div class="pm-overview" style="overflow: visible;">
+            <div class="pmo-pic">
+                <div >
+                    <a href="{{$article->url}}">
+                        <img class="img-responsive" src="{{$article->image_url}}" alt="">
+                    </a>
+                </div>
+            </div>
+
+            <div class="pmo-block" style="margin-top: 0px; padding-top:0px">
+                <h2>{{ $article->title }}</h2>
+                <b>{{$article->authors->implode('name', ', ')}}</b><br />
+                {{$article->published_at->toDateTimeString()}}
+            </div>
+        </div>
+
+        <div class="pm-body clearfix">
+            <div class="pmb-block">
+                <div class="pmbb-header">
+                    <h2><i class="zmdi zmdi-library m-r-5"></i> Article Information</h2>
+                </div>
+                <div class="pmbb-body p-l-30">
+                    <div class="pmbb-view">
+                        <dl class="dl-horizontal">
+                            <dt>External ID</dt>
+                            <dd>{{$article->external_id}}</dd>
+                        </dl>
+                        <dl class="dl-horizontal">
+                            <dt>URL</dt>
+                            <dd><a href="{{$article->url}}">{{$article->url}}</a></dd>
+                        </dl>
+                        <dl class="dl-horizontal">
+                            <dt>Title</dt>
+                            <dd>{{$article->title}}</dd>
+                        </dl>
+                        <dl class="dl-horizontal">
+                            <dt>Authors</dt>
+                            <dd>
+                                {{$article->authors->implode('name', ', ')}}
+                            </dd>
+                        </dl>
+                        <dl class="dl-horizontal">
+                            <dt>Published at</dt>
+                            <dd>
+                                {{$article->published_at->toDateTimeString()}}
+                            </dd>
+                        </dl>
+                        <dl class="dl-horizontal">
+                            <dt>Created at</dt>
+                            <dd>
+                                {{$article->created_at->toDateTimeString()}}
+                            </dd>
+                        </dl>
+                        <dl class="dl-horizontal">
+                            <dt>Updated at</dt>
+                            <dd>
+                                {{$article->updated_at->toDateTimeString()}}
+                            </dd>
+                        </dl>
+                    </div>
+                </div>
+            </div>
+
+            <div class="pmb-block">
+                <div class="pmbb-header">
+                    <h2><i class="zmdi zmdi-equalizer m-r-5"></i> Conversions</h2>
+                </div>
+                <div class="pmbb-body p-l-30">
+                    <div class="pmbb-view">
+                        <dl class="dl-horizontal">
+                            <dt>Total conversions</dt>
+                            <dd>{{$article->conversions->count()}}</dd>
+                        </dl>
+                        <dl class="dl-horizontal">
+                            <dt>
+                                <span data-toggle="tooltip" data-placement="top" title="" data-original-title="Ratio of new conversions and unique visitors">Conversion rate</span>
+                            </dt>
+                            <dd>{{$conversionRate}} %</dd>
+                        </dl>
+                        <dl class="dl-horizontal">
+                            <dt>New subscriptions</dt>
+                            <dd>{{$newSubscriptionsCount}}</dd>
+                        </dl>
+                        <dl class="dl-horizontal">
+                            <dt><span data-toggle="tooltip" data-placement="top" title="" data-original-title="Users who already had a subscription in the past">Renewed subscriptions</span></dt>
+                            <dd>{{$renewSubscriptionsCount}}</dd>
+                        </dl>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
     <div class="well">
@@ -25,12 +120,13 @@
     <div class="col-md-12">
         <div class="card">
             <div class="card-header">
-                <h2>Show article <small>{{ $article->title }}</small></h2>
+                <h2>Show article
+                    <small>{{ $article->title }}</small>
+                </h2>
             </div>
 
         </div>
     </div>
-
 
     <script type="text/javascript">
         new Vue({
