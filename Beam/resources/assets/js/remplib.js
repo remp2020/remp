a0af381fcff3feefcb93202556c7d24ccfb24264
Miroslav Svítok <mirosvitok@gmail.com>
2018-10-16 09:30:02 +0200

    Start tracking referrer in timespent tracking request

--- Beam/resources/assets/js/remplib.js
+++ Beam/resources/assets/js/remplib.js
@@ -464,7 +464,6 @@ remplib = typeof(remplib) === 'undefined' ? {} : remplib;
 
         timespentParamsCleanup: function(params) {
             delete params.user.url;
-            delete params.user.referer;
             delete params.user.user_agent;
             delete params.user.source.utm_source;
             delete params.user.source.utm_medium;
