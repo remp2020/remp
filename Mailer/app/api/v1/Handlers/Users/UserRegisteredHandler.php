dc8d4ab0cef97f780f23d8035fd2ae99929ed7cd
Miroslav Svítok <mirosvitok@gmail.com>
2018-11-12 14:46:08 +0100

    User registered Mailer API - this is a replacement for SubscribeWorkerCommand, which reads user registered events from Kafka. Instead, this will be replaced by direct API call.

---
+++ Mailer/app/api/v1/Handlers/Users/UserRegisteredHandler.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Remp\MailerModule\Api\v1\Handlers\Users;
+
+use Nette\Database\Table\ActiveRow;
+use Remp\MailerModule\Repository\ListsRepository;
+use Remp\MailerModule\Repository\UserSubscriptionsRepository;
+use Remp\MailerModule\User\IUser;
+use Tomaj\NetteApi\Handlers\BaseHandler;
+use Tomaj\NetteApi\Params\InputParam;
+use Tomaj\NetteApi\Response\JsonApiResponse;
+
+class UserRegisteredHandler extends BaseHandler
+{
+    private $userSubscriptionsRepository;
+
+    private $userProvider;
+
+    private $listsRepository;
+
+    public function __construct(
+        UserSubscriptionsRepository $userSubscriptionsRepository,
+        ListsRepository $listsRepository,
+        IUser $userProvider
+    ) {
+        parent::__construct();
+        $this->userSubscriptionsRepository = $userSubscriptionsRepository;
+        $this->userProvider = $userProvider;
+        $this->listsRepository = $listsRepository;
+    }
+
+    public function params()
+    {
+        return [
+            new InputParam(InputParam::TYPE_GET, 'user_id', InputParam::REQUIRED),
+        ];
+    }
+
+    public function handle($params)
+    {
+        $userId = $params['user_id'];
+
+        $userList = $this->userProvider->list([$userId], 1);
+        if (count($userList) === 0) {
+            return new JsonApiResponse(400, ['status' => 'error', 'message' => 'Invalid user_id parameter']);
+        }
+        $userInfo = $userList[$userId];
+
+        $lists = $this->listsRepository->all();
+
+        /** @var ActiveRow $list */
+        foreach ($lists as $list) {
+            if ($list->auto_subscribe) {
+                $this->userSubscriptionsRepository->subscribeUser($list, $userId, $userInfo['email']);
+            } else {
+                $this->userSubscriptionsRepository->unsubscribeUser($list, $userId, $userInfo['email']);
+            }
+        }
+
+        return new JsonApiResponse(200, ['status' => 'ok']);
+    }
+}
