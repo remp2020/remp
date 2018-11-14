c6bbc5b48d5cb2b81da73026faed7f5006854841
Miroslav Svítok <mirosvitok@gmail.com>
2018-11-14 13:55:30 +0100

    Increase title length in articles table

---
+++ Beam/database/migrations/2018_11_14_125025_increase_title_length_in_articles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class IncreaseTitleLengthInArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->string('title', 768)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            //
+        });
+    }
+}
