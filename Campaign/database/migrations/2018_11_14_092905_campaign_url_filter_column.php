618aaddb7bd7ea1c106ee8971819820b3a61588f
Adam Žoldák <zoldakadam@gmail.com>
2018-11-16 16:35:04 +0100

    campaign url filter change  migration
    remp/remp#331
    remove db default value and update all rows
    with raw query instead

--- Campaign/database/migrations/2018_11_14_092905_campaign_url_filter_column.php
+++ Campaign/database/migrations/2018_11_14_092905_campaign_url_filter_column.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Campaign;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -15,8 +16,16 @@ class CampaignUrlFilterColumn extends Migration
     public function up()
     {
         Schema::table('campaigns', function (Blueprint $table) {
-            $table->string('url_filter')->default(Campaign::URL_FILTER_EVERYWHERE);
+            $table->string('url_filter');
             $table->json('url_patterns')->nullable();
+        });
+
+        DB::statement('UPDATE campaigns SET url_filter = :filter', [
+            'filter' => Campaign::URL_FILTER_EVERYWHERE
+        ]);
+
+        Schema::table('campaigns', function (Blueprint $table) {
+            $table->string('url_filter')->nullable(false)->change();
         });
     }
 
