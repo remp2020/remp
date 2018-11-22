5703592f3bb24487bd12735c2241cf98dd4b7f5b
Peter Dulacka <dulacka@gmail.com>
2018-11-22 11:07:26 +0100

    Improving index speed of mail_user_subscriptions
    remp/remp#334

---
+++ Mailer/migrations/20181122100548_mailer_user_subscription_index.php
@@ -0,0 +1,22 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class MailerUserSubscriptionIndex extends AbstractMigration
+{
+    public function up()
+    {
+        $this->table('mail_user_subscriptions')
+            ->removeIndex(['mail_type_id'])
+            ->addIndex(['mail_type_id', 'subscribed'])
+            ->update();
+    }
+
+    public function down()
+    {
+        $this->table('mail_user_subscriptions')
+            ->removeIndex(['mail_type_id', 'subscribed'])
+            ->addIndex(['mail_type_id'])
+            ->update();
+    }
+}
