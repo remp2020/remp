4fdb07ecf9aa5d988b4838e302f66feeaab69222
Peter Dulacka <dulacka@gmail.com>
2018-12-06 11:53:03 +0100

    Removing throttling from SSO
    The reason for removal is that each service is expected to verify
    JWT token on each of its calls. Therefore there's an expectation,
    that most requests will come anonymously from a single IP.
    
    These requests would have been incorrectly throttled. As a simple
    solution we remove the throttling for now.

--- Sso/app/Http/Kernel.php
+++ Sso/app/Http/Kernel.php
@@ -37,7 +37,6 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:60,1',
             'bindings',
         ],
     ];
@@ -55,7 +54,6 @@ class Kernel extends HttpKernel
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
-        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
 
         'app.jwt.auth' => \App\Http\Middleware\VerifyUserToken::class,
         'jwt.refresh' => \Tymon\JWTAuth\Http\Middleware\RefreshToken::class,
