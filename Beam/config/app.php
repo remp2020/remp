547f17f288011961b21be68a0039540ac0c57e99
Miroslav Svítok <mirosvitok@gmail.com>
2018-12-12 13:33:19 +0100

    Beam Horizon installed

--- Beam/config/app.php
+++ Beam/config/app.php
@@ -153,6 +153,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\HorizonServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\RempJournalServiceProvider::class,
         App\Providers\RempMailerServiceProvider::class,
