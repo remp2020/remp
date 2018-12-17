0dc8ee2e3b5a41a2d0ba8ddf26bdf0afd0a381e5
Adam Žoldák <zoldakadam@gmail.com>
2018-12-17 15:30:49 +0100

    campaign showtime fix changed or deleted variants
    remp/remp#355

--- Campaign/app/Http/Controllers/CampaignController.php
+++ Campaign/app/Http/Controllers/CampaignController.php
@@ -578,6 +578,17 @@ class CampaignController extends Controller
                 }
             }
 
+            /** @var CampaignBanner $seenVariant */
+            // unset seen variant if it was deleted
+            if (!($seenVariant = $campaignBanners->get($variantUuid))) {
+                $variantUuid = null;
+            }
+
+            // unset seen variant if its proportion is 0%
+            if ($seenVariant && $seenVariant->proportion === 0) {
+                $variantUuid = null;
+            }
+
             // variant still not set, choose random variant
             if ($variantUuid === null) {
                 $variantsMapping = $campaign->getVariantsProportionMapping();
