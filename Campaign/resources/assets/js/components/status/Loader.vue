aaec5b2c44a03f5c3183ba60814bf36f73377517
Miroslav Svítok <mirosvitok@gmail.com>
2019-01-17 14:58:12 +0100

    Loader Vue component

---
+++ Campaign/resources/assets/js/components/status/Loader.vue
@@ -0,0 +1,19 @@
+<template>
+    <div v-if="yes" class="d-flex justify-content-center preloader pls-purple">
+        <svg class="pl-circular" viewBox="25 25 50 50">
+            <circle class="plc-path" cx="50" cy="50" r="20"></circle>
+        </svg>
+    </div>
+</template>
+
+<style>
+    .d-flex {
+        display: flex;
+    }
+</style>
+
+<script>
+    export default {
+        props: ['yes'],
+    }
+</script>
