e069796a09fa7086417d58dfbb00c7b5183b0f8b
Miroslav Svítok <mirosvitok@gmail.com>
2019-02-04 16:28:19 +0100

    List of author segments

--- Beam/app/Http/Controllers/AuthorSegmentsController.php
+++ Beam/app/Http/Controllers/AuthorSegmentsController.php
@@ -6,7 +6,12 @@ use App\Console\Commands\ComputeAuthorsSegments;
 use App\Console\Commands\CreateAuthorsSegments;
 use App\Http\Requests\AuthorSegmentsRequest;
 use App\Model\Config;
+use App\Segment;
+use App\SegmentGroup;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
+use Yajra\DataTables\DataTables;
 
 /**
  * Controller for testing author segments conditions, not shown in menu
@@ -14,6 +19,25 @@ use Illuminate\Support\Facades\Artisan;
  */
 class AuthorSegmentsController extends Controller
 {
+
+    public function index()
+    {
+        return view('authors.segments.index');
+    }
+
+    public function json(Request $request, Datatables $datatables)
+    {
+        $columns = ['id', 'name', 'code', 'created_at', 'updated_at',
+            DB::raw('(SELECT COUNT(*) FROM segment_users WHERE segment_users.segment_id=segments.id) as users_count'),
+            DB::raw('(SELECT COUNT(*) FROM segment_browsers WHERE segment_browsers.segment_id=segments.id) as browsers_count'),
+            ];
+        $segments = Segment::select($columns)
+            ->where('segment_group_id', SegmentGroup::getByCode(SegmentGroup::CODE_AUTHORS_SEGMENTS)->id);
+
+        return $datatables->of($segments)
+            ->make(true);
+    }
+
     public function test()
     {
         return view('authors.segments.test', [
