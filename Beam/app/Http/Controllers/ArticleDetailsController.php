8771cadb6c47f09fb8838f1d2b6448558e8d70a5
Miroslav Svítok <mirosvitok@gmail.com>
2019-02-05 10:35:48 +0100

    Decode HTML entities

--- Beam/app/Http/Controllers/ArticleDetailsController.php
+++ Beam/app/Http/Controllers/ArticleDetailsController.php
@@ -87,7 +87,9 @@ class ArticleDetailsController extends Controller
 
             $data['tagLabels'][$variant] = (object) [
                 'color' => $tagToColor[$variant],
-                'labels' => $variantTitles->pluck('title')->toArray(),
+                'labels' => $variantTitles->pluck('title')->map(function($title){
+                    return html_entity_decode($title, ENT_QUOTES);
+                })->toArray(),
             ];
         }
         return response()->json($data);
