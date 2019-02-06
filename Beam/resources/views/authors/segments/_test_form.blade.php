bf816872cb978dc7e8bb2738679416f1b5f87d4c
Miroslav Svítok <mirosvitok@gmail.com>
2019-02-06 16:41:35 +0100

    Improved configuration of author segments

--- Beam/resources/views/authors/segments/_test_form.blade.php
+++ Beam/resources/views/authors/segments/_test_form.blade.php
@@ -1,9 +1,33 @@
 <form method="post" action="{{ route('authorSegments.compute') }}">
 
-    <form-validator url="{{route('authorSegments.validateForm')}}"></form-validator>
+    <form-validator url="{{route('authorSegments.validateTest')}}"></form-validator>
 
     <div class="col-md-6">
         {{ csrf_field() }}
+
+        <p class="c-black f-500 m-b-10">Minimal ratio of (author articles/all articles) read by user:</p>
+
+        <div class="form-group">
+            <div class="fg-line">
+                <input id="min_ratio" class="form-control input-sm" value="{{ old('min_ratio') }}" name="min_ratio" required placeholder="e.g. 0.25 (value between 0.0 - 1.0)" type="number" step="0.01" min="0" max="1" />
+            </div>
+        </div>
+
+        <p class="c-black f-500 m-b-10">Minimal number of author articles read by user:</p>
+
+        <div class="form-group">
+            <div class="fg-line">
+                <input id="min_views" class="form-control input-sm" value="{{ old('min_views') }}" placeholder="e.g. 5" required name="min_views" min="0" type="number" />
+            </div>
+        </div>
+
+        <p class="c-black f-500 m-b-10">Minimal average time spent on author's articles by user (seconds):</p>
+
+        <div class="form-group">
+            <div class="fg-line">
+                <input id="min_average_timespent" class="form-control input-sm" value="{{ old('min_average_timespent') }}" required placeholder="e.g. 120 (value in seconds)" name="min_average_timespent" min="0" type="number" />
+            </div>
+        </div>
 
         <p class="c-black f-500 m-b-10">Use data from the last:</p>
 
@@ -31,29 +55,6 @@
             </label>
         </div>
 
-        <p class="c-black f-500 m-b-10"><b>X</b> - minimal ratio of (author articles/all articles) read by user:</p>
-
-        <div class="form-group">
-            <div class="fg-line">
-                <input id="min_ratio" class="form-control input-sm" value="{{ old('min_ratio') }}" name="min_ratio" required placeholder="e.g. {{$default_min_ratio}} (value between 0.0 - 1.0)" type="number" step="0.01" min="0" max="1" />
-            </div>
-        </div>
-
-        <p class="c-black f-500 m-b-10"><b>Y</b> - minimal number of author articles read by user:</p>
-
-        <div class="form-group">
-            <div class="fg-line">
-                <input id="min_views" class="form-control input-sm" value="{{ old('min_views') }}" placeholder="e.g. {{$default_min_views}}" required name="min_views" min="0" type="number" />
-            </div>
-        </div>
-
-        <p class="c-black f-500 m-b-10"><b>Z</b> - minimal average time spent on author's articles by user (in seconds):</p>
-
-        <div class="form-group">
-            <div class="fg-line">
-                <input id="min_average_timespent" class="form-control input-sm" value="{{ old('min_average_timespent') }}" required placeholder="e.g. {{$default_min_average_timespent}} (value in seconds)" name="min_average_timespent" min="0" type="number" />
-            </div>
-        </div>
 
         <div class="form-group">
             <div class="fg-line">
