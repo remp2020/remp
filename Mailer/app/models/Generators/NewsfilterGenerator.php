ef16bff31ab2f4e13eaea5d7cc48bdec85e3f60f
Adam Žoldák <zoldakadam@gmail.com>
2019-02-20 13:45:04 +0100

    newsfilter generator parse ols + fixes
    remp/web#700

--- Mailer/app/models/Generators/NewsfilterGenerator.php
+++ Mailer/app/models/Generators/NewsfilterGenerator.php
@@ -55,6 +55,8 @@ class NewsfilterGenerator implements IGenerator
         $content = $this->content;
 
         $post = $values->newsfilter_html;
+        $post = $this->parseOls($post);
+
         $lockedPost = $this->getLockedHtml($values->newsfilter_html, $values->url);
 
         list(
@@ -115,11 +117,13 @@ class NewsfilterGenerator implements IGenerator
 
             // replace ul & /ul
             '/<ul>/is' => '<table style="border-spacing:0;border-collapse:collapse;vertical-align:top;color:#181818;padding:0;margin:0;Margin:0;line-height:1.3;text-align:left;font-family:\'Helvetica Neue\', Helvetica, Arial;width:100%;"><tbody>',
+            '/<ol.*?>/is' => '<table style="border-spacing:0;border-collapse:collapse;vertical-align:top;color:#181818;padding:0;margin:0;Margin:0;line-height:1.3;text-align:left;font-family:\'Helvetica Neue\', Helvetica, Arial;width:100%;"><tbody>',
 
             '/<\/ul>/is' => '</tbody></table>' . PHP_EOL,
+            '/<\/ol>/is' => '</tbody></table>' . PHP_EOL,
 
             // replace li
-            '/<li>(.*?)<\/li>/is' => $liTemplate,
+            '/<li.*?>(.*?)<\/li>/is' => $liTemplate,
 
             // hr
             '/(<hr>|<hr \/>)/is' => $hrTemplate,
@@ -380,5 +384,36 @@ HTML;
             $spacerTemplate,
             $imageTemplate,
         ];
+    }
+
+    public function parseOls($post)
+    {
+        $ols = [];
+        preg_match_all('/<ol.*?>(.*?)<\/ol>/is', $post, $ols);
+
+        foreach ($ols[1] as $olContent) {
+            $olsLis = [];
+            $liNum = 1;
+            $newOlContent = '';
+
+            preg_match_all('/<li>(.*?)<\/li>/is', $olContent, $olsLis);
+
+
+            foreach ($olsLis[1] as $liContent) {
+                $newOlContent .= '
+    <tr style="padding:0;vertical-align:top;text-align:left;">
+        <td class="bullet" style="padding:0;vertical-align:top;text-align:left;font-size:18px;line-height:1.6;width:30px;border-collapse:collapse !important;">' . $liNum . '</td>
+        <td style="padding:0;vertical-align:top;text-align:left;font-size:18px;line-height:1.6;border-collapse:collapse !important;">
+            <p style="margin:0 0 0 26px;Margin:0 0 0 26px;color:#181818;padding:0;margin:0;Margin:0;line-height:1.3;font-size:18px;line-height:1.6;margin-bottom:26px;Margin-bottom:26px;line-height:160%;text-align:left;font-weight:normal;word-wrap:break-word;-webkit-hyphens:auto;-moz-hyphens:auto;hyphens:auto;border-collapse:collapse !important;">' . $liContent . '</p>
+        </td>
+    </tr>';
+
+                $liNum++;
+            }
+
+            $post = str_replace($olContent, $newOlContent, $post);
+        }
+
+        return $post;
     }
 }
