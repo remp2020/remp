8a486d85055e7cd4467053f23b2a8957713278db
Miroslav Svítok <mirosvitok@gmail.com>
2019-05-06 15:56:02 +0200

    Private constant

--- Beam/app/Http/Controllers/ArticleDetailsController.php
+++ Beam/app/Http/Controllers/ArticleDetailsController.php
@@ -14,7 +14,7 @@ use Remp\Journal\JournalContract;
 
 class ArticleDetailsController extends Controller
 {
-    const ALLOWED_INTERVALS = 'today,7days,30days,all';
+    private const ALLOWED_INTERVALS = 'today,7days,30days,all';
 
     private $journal;
 
