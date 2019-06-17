0b49955a1f786dc53fcf6dc6a968daeb2cf68c3d
Miroslav Svítok <mirosvitok@gmail.com>
2019-06-17 10:17:37 +0200

    Added missing positions config

--- Campaign/config/banners.php
+++ Campaign/config/banners.php
@@ -62,6 +62,11 @@ return [
                 'right' => '0px',
             ],
         ],
+        'center' => [
+            'name' => 'Center',
+            'style' => [
+            ]
+        ]
     ],
     'alignments' => [
         'left' => [
