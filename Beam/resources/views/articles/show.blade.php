0350dd53e3aeb28c5d93c97e473174873ab2ddc7
Miroslav Svítok <mirosvitok@gmail.com>
2019-07-11 15:06:13 +0200

    Prototype of UI for article referrers
    remp/remp#446

--- Beam/resources/views/articles/show.blade.php
+++ Beam/resources/views/articles/show.blade.php
@@ -162,4 +162,63 @@
         })
     </script>
 
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    <h2>Referer stats <small></small></h2>
+                </div>
+
+                {!! Widget::run('DataTable', [
+                    'colSettings' => [
+                        'medium' => [
+                            'header' => 'medium',
+                            'orderable' => false,
+                            'filter' => ['external'], // TODO
+                            'priority' => 1,
+                        ],
+                        'source' => [
+                            'header' => 'source',
+                            'orderable' => false,
+                            'filter' => ['google.sk'], // TODO
+                            'priority' => 1,
+                        ],
+                        'visits_count' => [
+                            'header' => 'visits count',
+                            'searchable' => false,
+                            'priority' => 1,
+                            'render' => 'number',
+                            'className' => 'text-right',
+                        ],
+                    ],
+                    'dataSource' => route('visitors.dtReferers'),
+                    'order' => [2, 'desc'],
+                    'requestParams' => [
+                        'visited_from' => '$(\'[name="visited_from"]\').val()',
+                        'visited_to' => '$(\'[name="visited_to"]\').val()',
+                        'tz' => 'Intl.DateTimeFormat().resolvedOptions().timeZone',
+                        'subscriber' => '$("[name=\"subscriber\"]:checked").val()',
+                    ],
+                    'refreshTriggers' => [
+                        [
+                            'event' => 'change',
+                            'selector' => '[name="visited_from"]'
+                        ],
+                        [
+                            'event' => 'change',
+                            'selector' => '[name="visited_to"]',
+                        ],
+                        [
+                            'event' => 'change',
+                            'selector' => '[name="subscriber"]',
+                        ],
+                    ],
+                ]) !!}
+
+            </div>
+        </div>
+    </div>
+
+
+
 @endsection
