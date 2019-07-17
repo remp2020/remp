c5f0d40181e75ca09da3943630366a8edf032552
Miroslav Svítok <mirosvitok@gmail.com>
2019-07-17 14:08:14 +0200

    Improved test
    remp/remp#442

--- Beam/tests/Unit/SnapshotHelperTest.php
+++ Beam/tests/Unit/SnapshotHelperTest.php
@@ -27,17 +27,25 @@ class SnapshotHelperTest extends TestCase
         $shouldBeExcluded = [];
         $shouldBeKept = [];
 
+        // Window 0-4 min
         $shouldBeKept[] = factory(ArticleViewsSnapshot::class)->create(['time' => $start]);
         $shouldBeExcluded[] = factory(ArticleViewsSnapshot::class)->create(['time' => (clone $start)->addMinutes(1)]);
         $shouldBeExcluded[] = factory(ArticleViewsSnapshot::class)->create(['time' => (clone $start)->addMinutes(2)]);
         $shouldBeExcluded[] = factory(ArticleViewsSnapshot::class)->create(['time' => (clone $start)->addMinutes(3)]);
         $shouldBeExcluded[] = factory(ArticleViewsSnapshot::class)->create(['time' => (clone $start)->addMinutes(4)]);
 
+        // Window 5-9 min
         $shouldBeKept[] = factory(ArticleViewsSnapshot::class)->create(['time' => (clone $start)->addMinutes( 6)]);
         $shouldBeExcluded[] = factory(ArticleViewsSnapshot::class)->create(['time' => (clone $start)->addMinutes(7)]);
         $shouldBeExcluded[] = factory(ArticleViewsSnapshot::class)->create(['time' => (clone $start)->addMinutes(8)]);
 
-        $pointsToExclude = $this->snapshotHelpers->timePointsToExclude($start, (clone $start)->addMinutes(15), 5);
+        // Window 10-14 min
+        $shouldBeKept[] = factory(ArticleViewsSnapshot::class)->create(['time' => (clone $start)->addMinutes(10)]);
+
+        // Window 15-19 min
+        $shouldBeKept[] = factory(ArticleViewsSnapshot::class)->create(['time' => (clone $start)->addMinutes(16)]);
+
+        $pointsToExclude = $this->snapshotHelpers->timePointsToExclude($start, (clone $start)->addMinutes(20), 5);
 
         foreach ($shouldBeExcluded as $item) {
             $this->assertTrue(in_array($item->time->toIso8601ZuluString(), $pointsToExclude));
