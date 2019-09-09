14fff42c6f9b1543fef3ab4fd5ae2c6a8e4e52c2
Peter Dulacka <dulacka@gmail.com>
2019-09-09 11:13:17 +0200

    Simplifying utm parameter replacement
    As we're working with actual query parameters, it's easier (to read
    and implement) to just extract the URLs, change the parameters
    and then replace the original anchor with new one.

--- Mailer/app/models/Replace/UtmReplace.php
+++ Mailer/app/models/Replace/UtmReplace.php
@@ -2,10 +2,7 @@
 
 namespace Remp\MailerModule\Replace;
 
-use Carbon\Carbon;
-use GuzzleHttp\Client;
-use GuzzleHttp\Exception\ClientException;
-use GuzzleHttp\Exception\ConnectException;
+use Nette\Http\Url;
 
 class UtmReplace implements ReplaceInterface
 {
@@ -27,48 +24,25 @@ class UtmReplace implements ReplaceInterface
 
     public function replace($content)
     {
-        // avoid html entities on url
-        $content = html_entity_decode($content);
-        // prepare UTM parameters to insert into url
-        $urlString = $this->formatUrlString();
-        // extract query string from url
-        $query = preg_replace('/<a(.*?)href="([^"#?]*)([^"]*?)"(.*?)>/i', '$2$3', $content);
-        // remove undesired utm parameter from others systems
-        $query = $this->removeUtms($query);
-        // rebuild url with all above
-        $content = preg_replace('/<a(.*?)href="([^"#?]*)([^"]*?)"(.*?)>/i', '<a$1href="$2?' . $urlString . $query .'"$4>', $content);
+        $matches = [];
+        preg_match_all('/<a(.*?)href="([^"]*?)"(.*?)>/i', $content, $matches);
 
-        // make sure we don't have two "?" characters in query string
-        preg_match_all('/href="([^"]*)"/iU', $content, $matches);
+        if (count($matches) > 0) {
+            foreach ($matches[2] as $idx => $hrefUrl) {
+                if (strpos($hrefUrl, 'http') === false) {
+                    continue;
+                }
+                $url = new Url(html_entity_decode($hrefUrl));
+                $url->setQueryParameter('utm_source', $this->utmSource);
+                $url->setQueryParameter('utm_medium', $this->utmMedium);
+                $url->setQueryParameter('utm_campaign', $this->utmCampaign);
+                $url->setQueryParameter('utm_content', $this->utmContent);
 
-        foreach ($matches[0] as $match) {
-            $newHref = substr($match, 0, strpos($match, '?') + 1) . str_replace('?', '&', substr($match, strpos($match, '?') + 1));
-            $content = str_replace($match, $newHref, $content);
+                $href = sprintf('<a%shref="%s"%s>', $matches[1][$idx], $url->getAbsoluteUrl(), $matches[3][$idx]);
+                $content = str_replace($matches[0][$idx], $href, $content);
+            }
         }
 
         return $content;
     }
-
-    private function formatUrlString()
-    {
-        return "utm_source={$this->utmSource}&utm_medium={$this->utmMedium}&utm_campaign={$this->utmCampaign}&utm_content={$this->utmContent}";
-    }
-
-    private function removeUtms($url) {
-
-        $url_array = parse_url($url);
-        $query_array = [];
-        if(!empty($url_array['query'])){
-            $url_array['query'] = html_entity_decode($url_array['query']);
-            parse_str($url_array['query'], $query_array);
-            unset($query_array['utm_source']);
-            unset($query_array['utm_medium']);
-            unset($query_array['utm_campaign']);
-            unset($query_array['utm_content']);
-        }
-
-        return (count($query_array) > 0 ? '&':'').http_build_query($query_array);
-
-    }
-
 }
--- Mailer/tests/Unit/UtmReplaceTest.php
+++ Mailer/tests/Unit/UtmReplaceTest.php
@@ -11,43 +11,56 @@ class UtmReplaceTest extends TestCase
 
     public function setUp()
     {
-        // GIVEN || ARRANGE
         $this->utmReplace = new UtmReplace("demo-weekly-newsletter", "email", "impresa_mail_20190903103350", "");
     }
 
     public function testAddUtmParametersWhenTheUrlDoesNotContainAnyUTM()
     {
-        //WHEN || ACTION
-        $content = $this->utmReplace->replace("<a href=\"https://expresso.pt/html/que-nao-entrou-em-acao\" target=\"blank\"/>");
-
-        //THEN || ASSERT
-        $this->assertTrue(true,'<a href=\"https://expresso.pt/html/que-nao-entrou-em-acao?utm_source=demo-weekly-newsletter&utm_medium=email&utm_campaign=impresa_mail_20190903103350&utm_content=\" target=\"blank\"/>');
+        $content = $this->utmReplace->replace('<a href="https://expresso.pt/html/que-nao-entrou-em-acao" target="blank"/>');
+        $this->assertEquals(
+            '<a href="https://expresso.pt/html/que-nao-entrou-em-acao?utm_source=demo-weekly-newsletter&utm_medium=email&utm_campaign=impresa_mail_20190903103350&utm_content=" target="blank"/>',
+            $content
+        );
     }
 
     public function testReplaceExistingUTMParametersWhenTheUrlAlreadyContainsTheseUtm() {
-        //WHEN || ACTION
-        $content = $this->utmReplace->replace("<a href=\"https://expresso.pt/html/que-nao-entrou-em-acao?utm_content=apple&amp;feira=terca&modelo=1?a=1&b=2\" target=\"blank\"/>");
-
-        //THEN || ASSERT
-        $this->assertTrue($content ===  '<a href="https://expresso.pt/html/que-nao-entrou-em-acao?utm_source=demo-weekly-newsletter&utm_medium=email&utm_campaign=impresa_mail_20190903103350&utm_content=&feira=terca&modelo=1%3Fa%3D1&b=2" target="blank"/>');
+        $content = $this->utmReplace->replace('<a href="https://expresso.pt/html/que-nao-entrou-em-acao?utm_content=apple&amp;feira=terca&modelo=1?a=1&b=2" target="blank"/>');
+        $this->assertEquals(
+            '<a href="https://expresso.pt/html/que-nao-entrou-em-acao?utm_content=&feira=terca&modelo=1%3Fa%3D1&b=2&utm_source=demo-weekly-newsletter&utm_medium=email&utm_campaign=impresa_mail_20190903103350" target="blank"/>',
+            $content
+        );
     }
 
     public function testFixHTMLEntitiesFromURL() {
-        //WHEN || ACTION
-        $content = $this->utmReplace->replace("<a href=\"https://expresso.pt/html/que-nao-entrou-em-acao?utm_content=apple&amp;feira=terca&modelo=1?a=1&b=2\" target=\"blank\"/>");
-
-        //THEN || ASSERT
-        $this->assertTrue($content ===  '<a href="https://expresso.pt/html/que-nao-entrou-em-acao?utm_source=demo-weekly-newsletter&utm_medium=email&utm_campaign=impresa_mail_20190903103350&utm_content=&feira=terca&modelo=1%3Fa%3D1&b=2" target="blank"/>');
+        $content = $this->utmReplace->replace('<a href="https://expresso.pt/html/que-nao-entrou-em-acao?utm_content=apple&amp;feira=terca&modelo=1?a=1&b=2" target="blank"/>');
+        $this->assertEquals(
+            '<a href="https://expresso.pt/html/que-nao-entrou-em-acao?utm_content=&feira=terca&modelo=1%3Fa%3D1&b=2&utm_source=demo-weekly-newsletter&utm_medium=email&utm_campaign=impresa_mail_20190903103350" target="blank"/>',
+            $content
+        );
     }
 
     public function testPreserveTheParametersThatIsNotUTM() {
-        //WHEN || ACTION
-        $content = $this->utmReplace->replace("<a href=\"https://expresso.pt/html/que-nao-entrou-em-acao?feira=terca&modelo=1&a=1&b=2\" target=\"blank\"/>");
-
-        //THEN || ASSERT
-        $this->assertTrue($content ===  '<a href="https://expresso.pt/html/que-nao-entrou-em-acao?utm_source=demo-weekly-newsletter&utm_medium=email&utm_campaign=impresa_mail_20190903103350&utm_content=&feira=terca&modelo=1&a=1&b=2" target="blank"/>');
+        $content = $this->utmReplace->replace('<a href="https://expresso.pt/html/que-nao-entrou-em-acao?feira=terca&modelo=1&a=1&b=2" target="blank"/>');
+        $this->assertEquals(
+            '<a href="https://expresso.pt/html/que-nao-entrou-em-acao?feira=terca&modelo=1&a=1&b=2&utm_source=demo-weekly-newsletter&utm_medium=email&utm_campaign=impresa_mail_20190903103350&utm_content=" target="blank"/>',
+            $content
+        );
     }
 
+    public function testPreserveAdditionalAnchorAttributes() {
+        $content = $this->utmReplace->replace('<a style="color: red" data-foo="bar" href="https://expresso.pt/" target="blank"/>');
+        $this->assertEquals(
+            '<a style="color: red" data-foo="bar" href="https://expresso.pt/?utm_source=demo-weekly-newsletter&utm_medium=email&utm_campaign=impresa_mail_20190903103350&utm_content=" target="blank"/>',
+            $content
+        );
+    }
 
+    public function testPreserveMailtoLinks() {
+        $content = $this->utmReplace->replace('<a href="mailto:admin@example.com"/>');
+        $this->assertEquals(
+            '<a href="mailto:admin@example.com"/>',
+            $content
+        );
+    }
 
 }
