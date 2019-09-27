4f3b762656c2001c017a920e391f114f4fba734f
Miroslav Svítok <mirosvitok@gmail.com>
2019-09-27 10:13:29 +0200

    Do not run JS if Beam is not configured
    remp/remp#505

--- Campaign/resources/views/campaigns/stats.blade.php
+++ Campaign/resources/views/campaigns/stats.blade.php
@@ -29,6 +29,51 @@
                         :timezone="timezone"
                 ></campaign-stats-root>
         </div>
+
+        <script type="text/javascript">
+            new Vue({
+                el: "#smart-range-selector",
+                components: {
+                    SmartRangeSelector
+                },
+                methods: {
+                    callback: function (from, to) {
+                        $('[name="from"]').val(from).trigger("change");
+                        $('[name="to"]').val(to).trigger("change");
+                    }
+                }
+            });
+
+            new Vue({
+                el: "#stats-app",
+                components: {
+                    CampaignStatsRoot
+                },
+                mounted: function() {
+                    var vm = this;
+
+                    $('[name="from"]').on('change', function () {
+                        vm.from = $('[name="from"]').val();
+                    });
+
+                    $('[name="to"]').on('change', function () {
+                        vm.to = $('[name="to"]').val();
+                    });
+                },
+                data() {
+                    return {
+                        name: "<a href=\"{!! route('campaigns.show', ['campaign' => $campaign]) !!}\">{{ $campaign->name }}</a>",
+                        url: "{!! route('campaigns.stats.data', $campaign->id) !!}",
+                        variants: {!! @json($variants) !!},
+                        variantBannerLinks: {!! @json($variantBannerLinks) !!},
+                        variantBannerTexts: {!! @json($variantBannerTexts) !!},
+                        from: '{!! $from !!}',
+                        to: '{!! $to !!}',
+                        timezone: Intl.DateTimeFormat().resolvedOptions().timeZone
+                    }
+                }
+            })
+        </script>
     @else
         <div class="card">
             <div class="row">
@@ -39,49 +84,4 @@
             </div>
         </div>
     @endif
-
-    <script type="text/javascript">
-        new Vue({
-            el: "#smart-range-selector",
-            components: {
-                SmartRangeSelector
-            },
-            methods: {
-                callback: function (from, to) {
-                    $('[name="from"]').val(from).trigger("change");
-                    $('[name="to"]').val(to).trigger("change");
-                }
-            }
-        });
-
-        new Vue({
-            el: "#stats-app",
-            components: {
-                CampaignStatsRoot
-            },
-            mounted: function() {
-                var vm = this;
-
-                $('[name="from"]').on('change', function () {
-                    vm.from = $('[name="from"]').val();
-                });
-
-                $('[name="to"]').on('change', function () {
-                    vm.to = $('[name="to"]').val();
-                });
-            },
-            data() {
-                return {
-                    name: "<a href=\"{!! route('campaigns.show', ['campaign' => $campaign]) !!}\">{{ $campaign->name }}</a>",
-                    url: "{!! route('campaigns.stats.data', $campaign->id) !!}",
-                    variants: {!! @json($variants) !!},
-                    variantBannerLinks: {!! @json($variantBannerLinks) !!},
-                    variantBannerTexts: {!! @json($variantBannerTexts) !!},
-                    from: '{!! $from !!}',
-                    to: '{!! $to !!}',
-                    timezone: Intl.DateTimeFormat().resolvedOptions().timeZone
-                }
-            }
-        })
-    </script>
 @endsection
