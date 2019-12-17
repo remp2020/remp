51f3eff9c52c59db20b605afa555276bd4e26558
Miroslav Svítok <mirosvitok@gmail.com>
2019-12-17 10:07:59 +0100

    Fixed typo in required attribute
    remp/remp#562

--- Campaign/resources/assets/js/components/templates/MediumRectangle.vue
+++ Campaign/resources/assets/js/components/templates/MediumRectangle.vue
@@ -77,7 +77,7 @@
                             <span class="input-group-addon"><i class="zmdi zmdi-text-format"></i></span>
                             <div class="fg-line">
                                 <label for="main_text" class="fg-label">Main text</label>
-                                <input v-model="mainText" class="form-control fg-input" name="main_text" id="main_text" type="text" requried>
+                                <input v-model="mainText" class="form-control fg-input" name="main_text" id="main_text" type="text" required>
                             </div>
                             <div><small v-html="$parent.fieldParamsMessage"></small></div>
                         </div>
@@ -86,7 +86,7 @@
                             <span class="input-group-addon"><i class="zmdi zmdi-text-format"></i></span>
                             <div class="fg-line">
                                 <label for="button_text" class="fg-label">Button text</label>
-                                <input v-model="buttonText" class="form-control fg-input" name="button_text" id="button_text" type="text" requried>
+                                <input v-model="buttonText" class="form-control fg-input" name="button_text" id="button_text" type="text" required>
                             </div>
                         </div>
                     </div>
