ffb4a55da44145a40999339000d09b6e3e449c35
Miroslav Svítok <mirosvitok@gmail.com>
2020-09-20 10:04:53 +0200

    Make sure that campaign is not shown if invalid seen campaign data is received on showtime
    remp/remp#715

--- Campaign/app/Http/Showtime/Showtime.php
+++ Campaign/app/Http/Showtime/Showtime.php
@@ -379,9 +379,19 @@ class Showtime
         // We need to track such campaigns on the client-side too.
         $activeCampaignUuids[] = $campaign->uuid;
 
+        $seenCampaign = $seenCampaigns->{$campaign->uuid} ?? null;
+
         // pageview rules - check display banner every n-th request
-        $pageviewCount = $seenCampaigns->{$campaign->uuid}->count ?? null;
-        if ($pageviewCount !== null && $campaign->pageview_rules !== null) {
+        if ($seenCampaign !== null && $campaign->pageview_rules !== null) {
+            $pageviewCount =  $seenCampaign->count ?? null;
+
+            if ($pageviewCount === null) {
+                // if campaign is recorder as seen but has no pageview count,
+                // it means there is a probably old version or remplib cached on the client
+                // do not show campaign (browser should reload the library)
+                return null;
+            }
+
             $displayBanner = $campaign->pageview_rules['display_banner'];
             $displayBannerEvery = $campaign->pageview_rules['display_banner_every'] ?? 1;
             if ($displayBanner === 'every' && $pageviewCount % $displayBannerEvery !== 0) {
@@ -390,8 +400,16 @@ class Showtime
         }
 
         // seen count rules
-        $seenCount = $seenCampaigns->{$campaign->uuid}->seen ?? null;
-        if ($seenCount !== null && $campaign->pageview_rules !== null) {
+        if ($seenCampaign !== null && $campaign->pageview_rules !== null) {
+            $seenCount = $seenCampaign->count ?? null;
+
+            if ($seenCount === null) {
+                // if campaign is recorder as seen but has no pageview count,
+                // it means there is a probably old version or remplib cached on the client
+                // do not show campaign (browser should reload the library)
+                return null;
+            }
+
             $displayTimes = $campaign->pageview_rules['display_times'] ?? null;
             $displayNTimes = $campaign->pageview_rules['display_n_times'];
             if ($displayTimes && $seenCount >= $displayNTimes) {
