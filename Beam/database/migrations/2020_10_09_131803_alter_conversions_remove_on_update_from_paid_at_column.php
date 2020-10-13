4882a34127f37f39a072c010f4033d1df125cde3
Miroslav Svítok <mirosvitok@gmail.com>
2020-10-13 10:54:13 +0200

    Drop default value from `paid_at` column in Conversions table
    remp/remp#739

--- Beam/database/migrations/2020_10_09_131803_alter_conversions_remove_on_update_from_paid_at_column.php
+++ Beam/database/migrations/2020_10_09_131803_alter_conversions_remove_on_update_from_paid_at_column.php
@@ -10,6 +10,8 @@ class AlterConversionsRemoveOnUpdateFromPaidAtColumn extends Migration
         // https://github.com/laravel/framework/issues/16526
         $sql = 'ALTER TABLE `conversions` CHANGE `paid_at` `paid_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP';
         DB::update($sql);
+        $sql = 'ALTER TABLE `conversions` ALTER COLUMN `paid_at` DROP DEFAULT';
+        DB::update($sql);
     }
 
     public function down()
