9fefc16066499d0c68e2c1bbc4acd51759d254ae
Adam Žoldák <zoldakadam@gmail.com>
2020-10-27 15:26:43 +0100

    add default newsfilter values for tyzden v behu newsletter
    remp/remp#754

--- Mailer/app/forms/NewsfilterTemplateFormFactory.php
+++ Mailer/app/forms/NewsfilterTemplateFormFactory.php
@@ -81,7 +81,16 @@ class NewsfilterTemplateFormFactory
         $form->addHidden('locked_text_content');
         $form->addHidden('article_id');
 
-        if (isset($_POST['source_template_id']) && $_POST['source_template_id'] == 47) {
+        if (isset($_POST['source_template_id']) && $_POST['source_template_id'] == 48) {
+            $defaults = [
+                'name' => 'Týždeň v behu ' . date('j.n.Y'),
+                'code' => 'nwsf_beh_' . date('dmY'),
+                'mail_layout_id' => 33, // layout for subscribers
+                'locked_mail_layout_id' => 33, // layout for non-subscribers
+                'mail_type_id' => 33, // Týždeň v behu
+                'from' => 'Denník N <info@dennikn.sk>',
+            ];
+        } elseif (isset($_POST['source_template_id']) && $_POST['source_template_id'] == 47) {
             $defaults = [
                 'name' => 'Grandslamové turnaje ' . date('j.n.Y'),
                 'code' => 'nwsf_grandslam_' . date('dmY'),
