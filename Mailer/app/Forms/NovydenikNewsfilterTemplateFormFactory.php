16c91421e75497bdff3fd1ec8bff45a75de858c0
Adam Žoldák <zoldakadam@gmail.com>
2021-01-20 15:33:30 +0100

    added cz denik politikon newsfilter default values
    remp/novydenik#731

--- Mailer/app/Forms/NovydenikNewsfilterTemplateFormFactory.php
+++ Mailer/app/Forms/NovydenikNewsfilterTemplateFormFactory.php
@@ -83,7 +83,16 @@ class NovydenikNewsfilterTemplateFormFactory
         $form->addHidden('locked_text_content');
         $form->addHidden('article_id');
 
-        if (isset($_POST['source_template_id']) && $_POST['source_template_id'] == 16) {
+        if (isset($_POST['source_template_id']) && $_POST['source_template_id'] == 17) {
+            $defaults = [
+                'name' => 'PolitikoN ' . date('j.n.Y'),
+                'code' => 'nwsf_politikon_' . date('dmY'),
+                'mail_layout_id' => 2, // empty layout
+                'locked_mail_layout_id' => 2, // empty layout
+                'mail_type_id' => 14, // Politikon
+                'from' => 'Deník N <info@denikn.cz>',
+            ];
+        } elseif (isset($_POST['source_template_id']) && $_POST['source_template_id'] == 16) {
             $defaults = [
                 'name' => 'Americký týdeník ' . date('j.n.Y'),
                 'code' => 'nwsf_amer_tydenik_' . date('dmY'),
