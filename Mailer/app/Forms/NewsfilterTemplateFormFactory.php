7be31ca92d012e24ae0ee8af0357d0d50a839729
Adam Žoldák <zoldakadam@gmail.com>
2021-03-23 15:07:55 +0100

    added default values for kosicky newsfilter newsletter
    remp/remp#857

--- Mailer/app/Forms/NewsfilterTemplateFormFactory.php
+++ Mailer/app/Forms/NewsfilterTemplateFormFactory.php
@@ -82,7 +82,16 @@ class NewsfilterTemplateFormFactory
         $form->addHidden('locked_text_content');
         $form->addHidden('article_id');
 
-        if (isset($_POST['source_template_id']) && $_POST['source_template_id'] == 48) {
+        if (isset($_POST['source_template_id']) && $_POST['source_template_id'] == 49) {
+            $defaults = [
+                'name' => 'Košický newsfilter ' . date('j.n.Y'),
+                'code' => 'nwsf_kosice_' . date('dmY'),
+                'mail_layout_id' => 33, // layout for subscribers
+                'locked_mail_layout_id' => 33, // layout for non-subscribers
+                'mail_type_id' => 34, // Kosickz newsfilter
+                'from' => 'Denník N <info@dennikn.sk>',
+            ];
+        } elseif (isset($_POST['source_template_id']) && $_POST['source_template_id'] == 48) {
             $defaults = [
                 'name' => 'Týždeň v behu ' . date('j.n.Y'),
                 'code' => 'nwsf_beh_' . date('dmY'),
