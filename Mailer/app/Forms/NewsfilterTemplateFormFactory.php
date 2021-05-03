627a72209083c5a19f4828117de12983f98f49d7
Adam Žoldák <zoldakadam@gmail.com>
2021-05-03 16:05:44 +0200

    add default values for dennikn porazeni newsfilter newsletter
    remp/remp#892

--- Mailer/app/Forms/NewsfilterTemplateFormFactory.php
+++ Mailer/app/Forms/NewsfilterTemplateFormFactory.php
@@ -82,7 +82,16 @@ class NewsfilterTemplateFormFactory
         $form->addHidden('locked_text_content');
         $form->addHidden('article_id');
 
-        if (isset($_POST['source_template_id']) && $_POST['source_template_id'] == 49) {
+        if (isset($_POST['source_template_id']) && $_POST['source_template_id'] == 50) {
+            $defaults = [
+                'name' => 'Porazení ' . date('j.n.Y'),
+                'code' => 'nwsf_porazeni_' . date('dmY'),
+                'mail_layout_id' => 33, // layout for subscribers
+                'locked_mail_layout_id' => 33, // layout for non-subscribers
+                'mail_type_id' => 35, // Porazeni newsfilter
+                'from' => 'Denník N <info@dennikn.sk>',
+            ];
+        } elseif (isset($_POST['source_template_id']) && $_POST['source_template_id'] == 49) {
             $defaults = [
                 'name' => 'Košický newsfilter ' . date('j.n.Y'),
                 'code' => 'nwsf_kosice_' . date('dmY'),
