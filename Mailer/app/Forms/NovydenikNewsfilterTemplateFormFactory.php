c5789e724b33e105a5c49e8abfc56400a90bda1d
Adam Žoldák <zoldakadam@gmail.com>
2021-07-30 12:46:08 +0200

    Added cinska depese newsletter default values for novy denik
    remp/novydenik#808

--- Mailer/app/Forms/NovydenikNewsfilterTemplateFormFactory.php
+++ Mailer/app/Forms/NovydenikNewsfilterTemplateFormFactory.php
@@ -83,7 +83,16 @@ class NovydenikNewsfilterTemplateFormFactory
         $form->addHidden('locked_text_content');
         $form->addHidden('article_id');
 
-        if (isset($_POST['source_template_id']) && $_POST['source_template_id'] == 21) {
+        if (isset($_POST['source_template_id']) && $_POST['source_template_id'] == 23) {
+            $defaults = [
+                'name' => 'Čínská depeše ' . date('j.n.Y'),
+                'code' => 'nwsf_cinska_depese_' . date('dmY'),
+                'mail_layout_id' => 2, // empty layout
+                'locked_mail_layout_id' => 2, // empty layout
+                'mail_type_id' => 17, // Čínská depeše
+                'from' => 'Deník N <info@denikn.cz>',
+            ];
+        } elseif (isset($_POST['source_template_id']) && $_POST['source_template_id'] == 21) {
             $defaults = [
                 'name' => 'Brněnský orloj ' . date('j.n.Y'),
                 'code' => 'nwsf_brnensky_orloj_' . date('dmY'),
