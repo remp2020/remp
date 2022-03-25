4eee0c7906d3e7aa93e236505e31ec8e896af04b
Adam Žoldák <zoldakadam@gmail.com>
2022-03-25 09:23:51 +0100

    Added default values for newsfilter ceska inteligence
    remp/novydenik#909

--- Mailer/app/Forms/NovydenikNewsfilterTemplateFormFactory.php
+++ Mailer/app/Forms/NovydenikNewsfilterTemplateFormFactory.php
@@ -93,7 +93,16 @@ class NovydenikNewsfilterTemplateFormFactory
         $form->addHidden('locked_text_content');
         $form->addHidden('article_id');
 
-        if (isset($_POST['source_template_id']) && $_POST['source_template_id'] == 24) {
+        if (isset($_POST['source_template_id']) && $_POST['source_template_id'] == 25) {
+            $defaults = [
+                'name' => 'Česká inteligence 20. století ' . date('j.n.Y'),
+                'code' => 'nwsf_ceska_inteligence_' . date('dmY'),
+                'mail_layout_id' => 2, // empty layout
+                'locked_mail_layout_id' => 2, // empty layout
+                'mail_type_id' => 19, // Česká inteligence 20. století
+                'from' => 'Deník N <info@denikn.cz>',
+            ];
+        } elseif (isset($_POST['source_template_id']) && $_POST['source_template_id'] == 24) {
             $defaults = [
                 'name' => 'Přepište dějiny ' . date('j.n.Y'),
                 'code' => 'nwsf_prepiste_dejiny_' . date('dmY'),
