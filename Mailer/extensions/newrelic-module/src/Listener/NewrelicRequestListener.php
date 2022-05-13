fc458051cef10190eac22112c01fc63fdcfe794d
Adam Žoldák <zoldakadam@gmail.com>
2022-05-13 11:20:30 +0000

    Created `NewrelicModule` extension
    remp/remp#1115
    + Added `NewrelicRequestListener` which handles naming request transactions

---
+++ Mailer/extensions/newrelic-module/src/Listener/NewrelicRequestListener.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Remp\NewrelicModule\Listener;
+
+use Nette\Application\Application;
+use Nette\Application\Request;
+use Remp\Mailer\Bootstrap;
+use Tracy\Debugger;
+use Tracy\ILogger;
+
+final class NewrelicRequestListener
+{
+    public function onRequest(Application $application, Request $request)
+    {
+        if (!extension_loaded('newrelic')) {
+            Debugger::log("You're using Newrelic module without 'newrelic' PHP extension. Either install the extension or disable the module in app's configuration.", ILogger::WARNING);
+            return;
+        }
+
+        if (Bootstrap::isCli()) {
+            newrelic_name_transaction('$ ' . basename($_SERVER['argv'][0]) . ' ' . implode(' ', array_slice($_SERVER['argv'], 1)));
+            newrelic_background_job(true);
+            return;
+        }
+
+        $params = $request->getParameters();
+        $name = $request->getPresenterName() . (isset($params['action']) ? ':' . $params['action'] : '');
+
+        // All API requests are routed through same presenter, append endpoint url.
+        if ($name === 'Api:Api:default') {
+            $apiEndpoint = 'api/v' . implode('/', [
+                    $params['version'] ?? '',
+                    $params['package'] ?? '',
+                    $params['apiAction'] ?? '',
+                ]);
+            $name .= " ({$apiEndpoint})";
+        }
+
+        newrelic_name_transaction($name);
+    }
+}
