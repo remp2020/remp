12be5721d38c9dca4e1824f09a5874837ccaf65f
Peter Dulacka <dulacka@gmail.com>
2022-09-09 15:21:09 +0200

    Fix "show campaign" button label

--- Campaign/resources/views/schedule/edit.blade.php
+++ Campaign/resources/views/schedule/edit.blade.php
@@ -12,7 +12,7 @@
             <h2>Edit scheduled run <small>{{ $schedule->campaign->name }}</small></h2>
             <div class="actions">
                 <a href="{{ route('campaigns.show', ['campaign' => $schedule->campaign])  }}" class="btn palette-Cyan bg waves-effect">
-                    <i class="zmdi zmdi-palette-Cyan zmdi-edit"></i> Edit campaign
+                    <i class="zmdi zmdi-palette-Cyan zmdi-eye"></i> Show campaign
                 </a>
                 <a href="{{ route('campaigns.edit', ['campaign' => $schedule->campaign])  }}" class="btn palette-Cyan bg waves-effect">
                     <i class="zmdi zmdi-palette-Cyan zmdi-edit"></i> Edit campaign
