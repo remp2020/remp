bb4b19aa5644d1c699fe1bd2c825bfa38581ca7d
Adam Žoldák <zoldakadam@gmail.com>
2022-12-30 14:15:42 +0000

    Added new generator `DailyMinuteGenerator`
    remp/remp#885
    - added WordpressBlockParser class which parses wordpress
    JSON input with specified blocks
    - added DailyMinuteTempleteFormFactory and DailyMinuteWidget for
    generating mail jobs

---
+++ Mailer/app/Forms/DailyMinuteTemplateFormFactory.php
@@ -0,0 +1,130 @@
+<?php
+declare(strict_types=1);
+
+namespace Remp\Mailer\Forms;
+
+use Nette\Application\UI\Form;
+use Nette\Forms\Controls\SubmitButton;
+use Nette\Security\User;
+use Remp\MailerModule\Models\Auth\PermissionManager;
+use Remp\MailerModule\Models\Segment\Crm;
+use Remp\MailerModule\Repositories\BatchesRepository;
+use Remp\MailerModule\Repositories\JobsRepository;
+use Remp\MailerModule\Repositories\LayoutsRepository;
+use Remp\MailerModule\Repositories\ListsRepository;
+use Remp\MailerModule\Repositories\TemplatesRepository;
+
+class DailyMinuteTemplateFormFactory
+{
+    private const FORM_ACTION_WITH_JOBS_CREATED = 'generate_emails_jobs_created';
+    private const FORM_ACTION_WITH_JOBS_STARTED = 'generate_emails_jobs';
+
+    public $onUpdate;
+
+    public $onSave;
+
+    public function __construct(
+        private string $segment,
+        private TemplatesRepository $templatesRepository,
+        private LayoutsRepository $layoutsRepository,
+        private ListsRepository $listsRepository,
+        private JobsRepository $jobsRepository,
+        private BatchesRepository $batchesRepository,
+        private PermissionManager $permissionManager,
+        private User $user
+    ) {
+    }
+
+    public function create()
+    {
+        $form = new Form;
+        $form->addProtection();
+
+        $form->addText('name', 'Name')
+            ->setRequired("Field 'Name' is required.");
+
+        $form->addText('code', 'Identifier')
+            ->setRequired("Field 'Identifier' is required.");
+
+        $form->addSelect('mail_layout_id', 'Template', $this->layoutsRepository->all()->fetchPairs('id', 'name'));
+
+        $mailTypes = $this->listsRepository->all()->where(['public_listing' => true])->fetchPairs('id', 'code');
+
+        $form->addSelect('mail_type_id', 'Type', $mailTypes)
+            ->setRequired("Field 'Type' is required.");
+
+        $form->addText('from', 'Sender')
+            ->setHtmlAttribute('placeholder', 'e.g. info@domain.com')
+            ->setRequired("Field 'Sender' is required.");
+
+        $form->addText('subject', 'Subject')
+            ->setRequired("Field 'Subject' is required.");
+
+        $form->addHidden('html_content');
+        $form->addHidden('text_content');
+
+        $form->setDefaults([
+            'name' => 'Daily minute ' . date('j.n.Y'),
+            'code' => 'daily_minute_' . date('dmY'),
+            'mail_layout_id' => 33, // layout for subscribers
+            'mail_type_id' => 8, // Denný súhrn z Minúty po minúte
+            'from' => 'Denník N <info@dennikn.sk>',
+        ]);
+
+        if ($this->permissionManager->isAllowed($this->user, 'batch', 'start')) {
+            $withJobs = $form->addSubmit(self::FORM_ACTION_WITH_JOBS_STARTED);
+            $withJobs->getControlPrototype()
+                ->setName('button')
+                ->setHtml('Generate newsletter batch and start sending');
+        }
+
+        $withJobsCreated = $form->addSubmit(self::FORM_ACTION_WITH_JOBS_CREATED);
+        $withJobsCreated->getControlPrototype()
+            ->setName('button')
+            ->setHtml('Generate newsletter batch');
+
+        $form->onSuccess[] = [$this, 'formSucceeded'];
+        return $form;
+    }
+
+    public function formSucceeded(Form $form, $values)
+    {
+        $generate = function ($htmlBody, $textBody, $mailLayoutId, $segmentCode = null) use ($values, $form) {
+            $mailTemplate = $this->templatesRepository->add(
+                $values['name'],
+                $this->templatesRepository->getUniqueTemplateCode($values['code']),
+                '',
+                $values['from'],
+                $values['subject'],
+                $textBody,
+                $htmlBody,
+                $mailLayoutId,
+                $values['mail_type_id']
+            );
+
+            $jobContext = 'daily_minute.' . date('Ymd');
+            $mailJob = $this->jobsRepository->add($segmentCode, Crm::PROVIDER_ALIAS, $jobContext);
+            $batch = $this->batchesRepository->add($mailJob->id, null, null, BatchesRepository::METHOD_RANDOM);
+            $this->batchesRepository->addTemplate($batch, $mailTemplate);
+
+            $batchStatus = BatchesRepository::STATUS_READY_TO_PROCESS_AND_SEND;
+
+            /** @var SubmitButton $withJobsCreatedSubmit */
+            $withJobsCreatedSubmit = $form[self::FORM_ACTION_WITH_JOBS_CREATED];
+            if ($withJobsCreatedSubmit->isSubmittedBy()) {
+                $batchStatus = BatchesRepository::STATUS_CREATED;
+            }
+
+            $this->batchesRepository->updateStatus($batch, $batchStatus);
+        };
+
+        $generate(
+            $values['html_content'],
+            $values['text_content'],
+            $values['mail_layout_id'],
+            $this->segment
+        );
+
+        $this->onSave->__invoke();
+    }
+}
---
+++ Mailer/app/Models/Generators/WordpressBlockParser.php
@@ -0,0 +1,133 @@
+<?php
+declare(strict_types=1);
+
+namespace Remp\Mailer\Models\Generators;
+
+use Remp\MailerModule\Models\ContentGenerator\Engine\EngineFactory;
+use Remp\MailerModule\Models\ContentGenerator\Engine\IEngine;
+use Remp\MailerModule\Models\ContentGenerator\Engine\TwigEngine;
+
+class WordpressBlockParser
+{
+    public const BLOCK_CORE_GROUP = 'core/group';
+    public const BLOCK_CORE_HEADING = 'core/heading';
+    public const BLOCK_CORE_PARAGRAPH = 'core/paragraph';
+    public const BLOCK_CORE_LIST = 'core/list';
+    public const BLOCK_CORE_COLUMN = 'core/column';
+    public const BLOCK_CORE_COLUMNS = 'core/columns';
+    public const BLOCK_CORE_IMAGE = 'core/image';
+    public const BLOCK_CORE_LIST_ITEM = 'core/list-item';
+    public const BLOCK_CORE_EMBED = 'core/embed';
+
+    public const BLOCK_DN_MINUTE = 'dn-newsletter/r5m-minute';
+
+    /** @var TwigEngine $twig */
+    private IEngine $twig;
+
+    private int $minuteOrderCounter = 1;
+
+    private bool $isFirstDNMinuteInGroup = true;
+
+    public function __construct(EngineFactory $engineFactory)
+    {
+        $this->twig = $engineFactory->engine('twig');
+    }
+
+    public function parseJson(string $json)
+    {
+        $data = preg_replace('/[[:cntrl:]]/', '', $json);
+        $data = json_decode($data);
+
+        $result = '';
+        $textResult = '';
+        foreach ($data as $block) {
+            $result .= $this->parseBlock($block);
+            $textResult .= strip_tags($result);
+        }
+
+        return [$result, $textResult];
+    }
+
+    public function getBlockTemplateData(object $block): array
+    {
+        $data = [
+            'originalContent' => $block->originalContent ?? null,
+            'content' => $block->attributes->content ?? null,
+            'verticalAlignment' => $block->attributes->verticalAlignment ?? null,
+            'fontSize' => $block->attributes->style->typography->fontSize ?? null,
+            'width' => $block->attributes->width ?? null,
+            'url' => $block->attributes->url ?? null,
+            'alt' => $block->attributes->alt ?? null,
+            'caption' => $block->attributes->caption ?? null,
+        ];
+
+        if ($block->name === self::BLOCK_CORE_GROUP
+            && isset($block->attributes->className)
+            && str_contains($block->attributes->className, 'wp-block-dn-newsletter-group-grey')
+        ) {
+            $data['group_grey'] = true;
+        }
+
+        if ($block->name === self::BLOCK_CORE_GROUP
+            && isset($block->attributes->className)
+            && str_contains($block->attributes->className, 'wp-block-dn-newsletter-group-ordered')
+        ) {
+            $data['group_ordered'] = true;
+        }
+
+        if ($block->name === self::BLOCK_DN_MINUTE) {
+            $data['isFirstDNMinuteInGroup'] = $this->isFirstDNMinuteInGroup;
+        }
+        return $data;
+    }
+
+    public function parseBlock(object $block, bool $groupOrdered = false): string
+    {
+        $params = [
+            'contents' => ''
+        ];
+
+        $params += $this->getBlockTemplateData($block);
+
+        if ($block->name === self::BLOCK_CORE_GROUP) {
+            $this->minuteOrderCounter = 1;
+            $this->isFirstDNMinuteInGroup = true;
+        } elseif ($block->name === self::BLOCK_CORE_HEADING) {
+            $this->isFirstDNMinuteInGroup = true;
+        } elseif ($block->name === self::BLOCK_DN_MINUTE) {
+            $this->isFirstDNMinuteInGroup = false;
+        }
+
+        if ($groupOrdered && $block->name === self::BLOCK_DN_MINUTE && $this->minuteOrderCounter < 6) {
+            $params['minuteOrderCounter'] = $this->minuteOrderCounter;
+            $this->minuteOrderCounter++;
+        }
+
+        $template = $this->getTemplate($block->name);
+        if (isset($block->innerBlocks) && !empty($block->innerBlocks)) {
+            foreach ($block->innerBlocks as $innerBlock) {
+                $params['contents'] .= $this->parseBlock($innerBlock, $params['group_ordered'] ?? false);
+            }
+        }
+        return $this->twig->render($template, $params);
+    }
+
+    public function getTemplate(string $blockName): string
+    {
+        $templateFile = match ($blockName) {
+            self::BLOCK_CORE_GROUP => __DIR__ . '/resources/templates/WordpressBlockParser/core-group.twig',
+            self::BLOCK_CORE_PARAGRAPH => __DIR__ . '/resources/templates/WordpressBlockParser/core-paragraph.twig',
+            self::BLOCK_CORE_HEADING => __DIR__ . '/resources/templates/WordpressBlockParser/core-heading.twig',
+            self::BLOCK_CORE_IMAGE => __DIR__ . '/resources/templates/WordpressBlockParser/core-image.twig',
+            self::BLOCK_CORE_COLUMN => __DIR__ . '/resources/templates/WordpressBlockParser/core-column.twig',
+            self::BLOCK_CORE_COLUMNS => __DIR__ . '/resources/templates/WordpressBlockParser/core-columns.twig',
+            self::BLOCK_CORE_LIST => __DIR__ . '/resources/templates/WordpressBlockParser/core-list.twig',
+            self::BLOCK_CORE_LIST_ITEM => __DIR__ . '/resources/templates/WordpressBlockParser/core-list-item.twig',
+            self::BLOCK_CORE_EMBED => __DIR__ . '/resources/templates/WordpressBlockParser/core-embed.twig',
+            self::BLOCK_DN_MINUTE => __DIR__ . '/resources/templates/WordpressBlockParser/dn-minute.twig',
+            default => throw new \Exception('not existing block template'),
+        };
+
+        return file_get_contents($templateFile);
+    }
+}
