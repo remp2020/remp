8f3997f0569e1e1adda9dbc1a6416763b53dff3f
Adam Žoldák <zoldakadam@gmail.com>
2023-01-05 17:10:19 +0100

    Refactored EmbedParser and WordpressBlockParser paragraph styling
    remp/remp#885

--- Mailer/app/Models/Generators/EmbedParser.php
+++ Mailer/app/Models/Generators/EmbedParser.php
@@ -9,18 +9,18 @@ class EmbedParser extends \Remp\MailerModule\Models\Generators\EmbedParser
     {
         $html = "<br>";
 
-        if ($isVideo) {
-            $html .= "<p style='text-align: center; font-weight: normal;'><i>{$this->videoLinkText}</i></p><br>";
-        }
-
-        $html .= "<a href='{$link}' target='_blank' style='color:#181818;padding:0;margin:0;Margin:0;line-height:1.3;text-decoration:none;text-align: center; display: block;'>";
+        $html .= "<a href='{$link}' target='_blank' style='color:#181818;padding:0;margin:0;Margin:0;line-height:1.3;text-decoration:none;text-align: center; display: block;width:100%;'>";
 
         if (!is_null($image)) {
-            $html .= "<img src='{$image}' alt='{$title}' style='outline:none;text-decoration:none;-ms-interpolation-mode:bicubic;width:auto;max-width:100%;clear:both;display:inline;'>";
+            $html .= "<img src='{$image}' alt='{$title}' style='outline:none;text-decoration:none;-ms-interpolation-mode:bicubic;max-width:100%;clear:both;display:inline;width:100%;height:auto;'>";
         } elseif (preg_match('/twitt/', $link)) {
             $html .= "<a style=\"display: block;margin: 0 0 20px;padding: 7px 10px;text-decoration: none;text-align: center;font-weight: bold;font-family:'Helvetica Neue', Helvetica, Arial;color: #249fdc; background: #ffffff; border: 3px solid #249fdc;margin: 16px 0 16px 0\" href=\"{$link}\" target=\"_blank\">Zobraziť na Twitteri</a>";
         } else {
             $html .= "<span style='text-decoration: underline; color: #1F3F83;'>" . $link . "</span>";
+        }
+
+        if ($isVideo) {
+            $html .= "<p style='color: #888;font-family: Arial,sans-serif;font-size: 14px;margin: 0; padding: 0;margin-top:5px;line-height: 1.3;text-align: left; text-decoration: none;'><i>{$this->videoLinkText}</i></p><br>";
         }
 
         return $html . "</a>" . PHP_EOL;
