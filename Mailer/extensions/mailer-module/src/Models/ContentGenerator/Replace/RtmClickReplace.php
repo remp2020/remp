006612fd9dc18dfafe5e8bfa532c331d039914aa
Matus Kalafut <matus.kalafut@projektn.sk>
2023-06-01 12:05:51 +0000

    Fix reported mail click tracker bugs
    - Fix mail click tracker to not modify URL in any way other
    than adding required query parameter.
    
    - Add support for horizontal scroll in DataTable
    (parameter `scrollX` in table settings).
    
    - Change `url` column type in `mail_template_links` table
    from `string` to `text` to support longer URLs.
    
    remp/remp#1270

--- Mailer/extensions/mailer-module/src/Models/ContentGenerator/Replace/RtmClickReplace.php
+++ Mailer/extensions/mailer-module/src/Models/ContentGenerator/Replace/RtmClickReplace.php
@@ -3,7 +3,6 @@
 namespace Remp\MailerModule\Models\ContentGenerator\Replace;
 
 use Nette\Caching\Storage;
-use Nette\Http\Url;
 use Remp\MailerModule\Models\Config\Config;
 use Remp\MailerModule\Repositories\MailTemplateLinksRepository;
 
@@ -24,8 +23,66 @@ abstract class RtmClickReplace implements IReplace
         return $this->config->get(self::CONFIG_NAME);
     }
 
-    protected function computeUrlHash(Url $url, string $additionalInfo = ''): string
+    protected function computeUrlHash(string $url, string $additionalInfo = ''): string
     {
-        return hash('crc32c', $url->setQuery([])->getAbsoluteUrl() . $additionalInfo);
+        return hash('crc32c', $url . $additionalInfo);
+    }
+
+    public function removeQueryParams(string $url): string
+    {
+        return explode('?', $url)[0];
+    }
+
+    public function setRtmClickHashInUrl(string $url, string $hash): string
+    {
+        $url = self::removeRtmClickHash($url);
+        $hashQueryParam = self::HASH_PARAM . '=' . $hash;
+
+        // url already has query params
+        if (isset(explode('?', $url)[1])) {
+            return "{$url}&{$hashQueryParam}";
+        }
+
+        return "{$url}?{$hashQueryParam}";
+    }
+
+    public static function getRtmClickHashFromUrl(string $url): ?string
+    {
+        $matches = [];
+        // Extracts RTM click hash value from URL query params
+        preg_match('/^[^?]*\??.*[?&]' . self::HASH_PARAM . '=([^?&\s]*).*$/m', $url, $matches);
+
+        return empty($matches[1]) ? null : $matches[1];
+    }
+
+    public static function removeRtmClickHash(string $url): string
+    {
+        $matches = [];
+        // Split URL between path and params (before and after '?')
+        preg_match('/^([^?]*)\??(.*)$/', $url, $matches);
+
+        $path = $matches[1];
+        $params = explode('&', $matches[2] ?? '');
+        $finalParams = [];
+
+        foreach ($params as $param) {
+            if (empty($param)) {
+                continue;
+            }
+
+            $items = explode('=', $param, 2);
+
+            if (strcasecmp($items[0], self::HASH_PARAM) === 0) {
+                continue;
+            }
+
+            $finalParams[] = $param;
+        }
+
+        if (empty($finalParams)) {
+            return $path;
+        }
+
+        return $path . '?' . implode('&', $finalParams);
     }
 }
