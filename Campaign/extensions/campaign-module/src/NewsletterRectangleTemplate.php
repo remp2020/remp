69dc556516eb7a1f1aae54c9b5cb02c8edf80376
Peter Dulacka <dulacka@gmail.com>
2025-03-19 10:17:39 +0100

    Fix config access issue in showtime
    Config class is not available in showtime request and
    attempt to access config value was causing errors
    (see related issue with full call stack).
    
    The config values are cached and we don't really need
    to read them from config. It's sufficient to read
    them from cache before attempting to read them
    from actual config.
    
    remp/remp#1401

--- Campaign/extensions/campaign-module/src/NewsletterRectangleTemplate.php
+++ Campaign/extensions/campaign-module/src/NewsletterRectangleTemplate.php
@@ -33,54 +33,39 @@ class NewsletterRectangleTemplate extends AbstractTemplate
         'remp_mailer_addr'
     ];
 
-    public function __construct(array $attributes = [])
-    {
-        $this->banner_config = [
-            'endpoint' => config('newsletter_banners.endpoint'),
-            'use_xhr' => config('newsletter_banners.use_xhr'),
-            'request_body' => config('newsletter_banners.request_body'),
-            'request_headers' => config('newsletter_banners.request_headers'),
-            'params_transposition' => config('newsletter_banners.params_transposition'),
-            'params_extra' => config('newsletter_banners.params_extra'),
-            'remp_mailer_addr' => config('services.remp.mailer.web_addr')
-        ];
-
-        parent::__construct($attributes);
-    }
-
     public function getEndpointAttribute()
     {
-        return $this->banner_config['endpoint'];
+        return $this->attributes['endpoint'] ?? $this->getConfig('endpoint');
     }
 
     public function getUseXhrAttribute()
     {
-        return $this->banner_config['use_xhr'];
+        return $this->attributes['use_xhr'] ?? $this->getConfig('use_xhr');
     }
 
     public function getRequestBodyAttribute()
     {
-        return $this->banner_config['request_body'];
+        return $this->attributes['request_body'] ?? $this->getConfig('request_body');
     }
 
     public function getRequestHeadersAttribute()
     {
-        return $this->banner_config['request_headers'];
+        return $this->attributes['request_headers'] ?? $this->getConfig('request_headers');
     }
 
     public function getParamsTranspositionAttribute()
     {
-        return $this->banner_config['params_transposition'];
+        return $this->attributes['params_transposition'] ?? $this->getConfig('params_transposition');
     }
 
     public function getParamsExtraAttribute()
     {
-        return $this->banner_config['params_extra'];
+        return $this->attributes['params_extra'] ?? $this->getConfig('params_extra');
     }
 
     public function getRempMailerAddrAttribute()
     {
-        return $this->banner_config['remp_mailer_addr'];
+        return $this->attributes['remp_mailer_addr'] ?? $this->getConfig('remp_mailer_addr');
     }
 
     /**
@@ -90,5 +75,22 @@ class NewsletterRectangleTemplate extends AbstractTemplate
     public function text()
     {
         return strip_tags("({$this->newsletter_id}) {$this->title} -- {$this->text}");
+    }
+
+    private function getConfig($key)
+    {
+        if (!isset($this->banner_config)) {
+            $this->banner_config = [
+                'endpoint' => config('newsletter_banners.endpoint'),
+                'use_xhr' => config('newsletter_banners.use_xhr'),
+                'request_body' => config('newsletter_banners.request_body'),
+                'request_headers' => config('newsletter_banners.request_headers'),
+                'params_transposition' => config('newsletter_banners.params_transposition'),
+                'params_extra' => config('newsletter_banners.params_extra'),
+                'remp_mailer_addr' => config('services.remp.mailer.web_addr')
+            ];
+        }
+
+        return $this->banner_config[$key] ?? null;
     }
 }
