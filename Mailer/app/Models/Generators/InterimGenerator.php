98342a43d88f60a60c65ccebd4652a9bd4adde5c
Lubos Michalik <lubos.michalik@projektn.sk>
2026-01-20 13:35:55 +0100

    Add interim generator and parser
    remp/remp#1438

---
+++ Mailer/app/Models/Generators/InterimGenerator.php
@@ -0,0 +1,127 @@
+<?php
+declare(strict_types=1);
+
+namespace Remp\Mailer\Models\Generators;
+
+use Nette\Application\UI\Form;
+use Nette\Utils\ArrayHash;
+use Remp\Mailer\Components\GeneratorWidgets\Widgets\DailyMinuteWidget\DailyMinuteWidget;
+use Remp\MailerModule\Models\ContentGenerator\Engine\EngineFactory;
+use Remp\MailerModule\Models\Generators\IGenerator;
+use Remp\MailerModule\Models\Generators\InvalidUrlException;
+use Remp\MailerModule\Models\Generators\PreprocessException;
+use Remp\MailerModule\Repositories\SourceTemplatesRepository;
+use Tomaj\NetteApi\Params\PostInputParam;
+
+class InterimGenerator implements IGenerator
+{
+    public $onSubmit;
+
+    public function __construct(
+        private SourceTemplatesRepository $sourceTemplatesRepository,
+        private EngineFactory $engineFactory,
+        private InterimWordpressBlockParser $wordpressBlockParser,
+    ) {
+    }
+
+    public function generateForm(Form $form): void
+    {
+        // disable CSRF protection as external sources could post the params here
+        $form->offsetUnset(Form::ProtectorId);
+
+        $form->addText('subject', 'Subject')
+            ->setHtmlAttribute('class', 'form-control')
+            ->setRequired();
+
+        $form->addText('from', 'From')
+            ->setHtmlAttribute('class', 'form-control');
+
+        $form->addText('url', 'URL')
+            ->setHtmlAttribute('class', 'form-control');
+
+        $form->addTextArea('blocks_json', 'Blocks JSON')
+            ->setHtmlAttribute('rows', 8)
+            ->setHtmlAttribute('class', 'form-control')
+            ->setRequired();
+
+        $form->onSuccess[] = [$this, 'formSucceeded'];
+    }
+
+    public function formSucceeded(Form $form, ArrayHash $values): void
+    {
+        try {
+            $output = $this->process((array)$values);
+
+            $addonParams = [
+                'render' => true,
+                'from' => $values->from,
+                'subject' => $values->subject,
+                'errors' => $output['errors'],
+            ];
+
+            $this->onSubmit->__invoke($output['htmlContent'], $output['textContent'], $addonParams);
+        } catch (InvalidUrlException $e) {
+            $form->addError($e->getMessage());
+        }
+    }
+
+    public function onSubmit(callable $onSubmit): void
+    {
+        $this->onSubmit = $onSubmit;
+    }
+
+    public function getWidgets(): array
+    {
+        return [DailyMinuteWidget::class];
+    }
+
+    public function apiParams(): array
+    {
+        return [
+            (new PostInputParam('url'))->setRequired(),
+            (new PostInputParam('subject'))->setRequired(),
+            (new PostInputParam('blocks_json'))->setRequired(),
+            (new PostInputParam('from')),
+        ];
+    }
+
+    public function process(array $values): array
+    {
+        [$html, $text] = $this->wordpressBlockParser->parseJson($values['blocks_json']);
+
+        $engine = $this->engineFactory->engine();
+        $sourceTemplate = $this->sourceTemplatesRepository->find($values['source_template_id']);
+
+        return [
+            'htmlContent' => $engine->render($sourceTemplate->content_html, ['html' => $html]),
+            'textContent' => $engine->render($sourceTemplate->content_text, ['text' => $text]),
+            'from' => $values['from'],
+            'subject' => $values['subject'],
+            'errors' => []
+        ];
+    }
+
+    public function preprocessParameters($data): ?ArrayHash
+    {
+        $output = new ArrayHash();
+
+        if (!isset($data->blocks)) {
+            throw new PreprocessException("WP json object does not contain required attribute 'blocks'");
+        }
+
+        if (!isset($data->subject)) {
+            throw new PreprocessException("WP json object does not contain required attribute 'subject'");
+        }
+
+        if (!isset($data->url)) {
+            throw new PreprocessException("WP json object does not contain required attribute 'url'");
+        }
+
+        $output->from = $this->from;
+        $output->blocks_json = $data->blocks;
+        $output->subject = $data->subject;
+        $output->url = $data->url;
+
+        return $output;
+    }
+}
---
+++ Mailer/app/Models/Generators/InterimWordpressBlockParser.php
@@ -0,0 +1,110 @@
+<?php
+declare(strict_types=1);
+
+namespace Remp\Mailer\Models\Generators;
+
+use Remp\MailerModule\Models\ContentGenerator\Engine\EngineFactory;
+use Remp\MailerModule\Models\ContentGenerator\Engine\IEngine;
+use Remp\MailerModule\Models\ContentGenerator\Engine\TwigEngine;
+
+class InterimWordpressBlockParser
+{
+    public const BLOCK_CORE_GROUP = 'core/group';
+    public const BLOCK_CORE_HEADING = 'core/heading';
+    public const BLOCK_CORE_PARAGRAPH = 'core/paragraph';
+    public const BLOCK_CORE_LIST = 'core/list';
+    public const BLOCK_CORE_LIST_ITEM = 'core/list-item';
+    public const BLOCK_CORE_IMAGE = 'core/image';
+
+    public const BLOCK_EO_POST = 'eo/post';
+
+    /** @var TwigEngine $twig */
+    private IEngine $twig;
+
+    public function __construct(
+        EngineFactory $engineFactory
+    ) {
+        $this->twig = $engineFactory->engine('twig');
+    }
+
+    public function parseJson(string $json): array
+    {
+        $data = preg_replace('/[[:cntrl:]]/', '', $json);
+        $data = json_decode($data);
+
+        $htmlResult = '';
+        $textResult = '';
+
+        foreach ($data as $block) {
+            $parsedBlock = $this->parseBlock($block);
+            $htmlResult .= $parsedBlock;
+
+            $textBlock = html_entity_decode(strip_tags($parsedBlock));
+            $textBlock = preg_replace('/\n(\s*)\n(\s*)\n/', "\n\n", $textBlock);
+            $textBlock = preg_replace('/^\s/', "", $textBlock);
+            $textBlock = str_replace('  ', " ", $textBlock);
+
+            $textResult .= $textBlock;
+        }
+
+        return [$htmlResult, $textResult];
+    }
+
+    public function parseBlock(object $block, array $innerBlockParams = []): string
+    {
+        $params['contents'] = '';
+        $params += $this->getBlockTemplateData($block, $innerBlockParams);
+
+        $template = $this->getTemplate($block->name);
+        if (isset($block->innerBlocks) && !empty($block->innerBlocks)) {
+            foreach ($block->innerBlocks as $innerBlock) {
+                $params['contents'] .= $this->parseBlock($innerBlock);
+            }
+        }
+        return $this->twig->render($template, $params);
+    }
+
+    public function getBlockTemplateData(object $block, array $innerBlockParams = []): array
+    {
+        $data = [
+            'originalContent' => $block->originalContent ?? null,
+            'content' => $block->attributes->content ?? null,
+            'url' => $block->attributes->url ?? null,
+            'href' => $block->attributes->href ?? null,
+            'alt' => $block->attributes->alt ?? null,
+        ];
+
+        if ($block->name === self::BLOCK_CORE_GROUP) {
+            $data['backgroundColor'] = match ($block->attributes->backgroundColor ?? null) {
+                'grey-2' => '#f9f9f9',
+                'orange-fade' => '#fbddd9',
+                default => '#ffffff',
+            };
+        }
+
+        if ($block->name === self::BLOCK_CORE_HEADING) {
+            $data['level'] = $block->attributes->level;
+            $data['fontSize'] = $data['level'] === 1 ? '20px' : '16px';
+            $data['color'] = $data['level'] === 2 ? '#f0523c' : '#32353a';
+        }
+
+        return $data;
+    }
+
+    public function getTemplate(string $blockName): string
+    {
+        $templateFile = match ($blockName) {
+            self::BLOCK_CORE_GROUP => __DIR__ . '/resources/templates/InterimWordpressBlockParser/core-group.twig',
+            self::BLOCK_CORE_HEADING => __DIR__ . '/resources/templates/InterimWordpressBlockParser/core-heading.twig',
+            self::BLOCK_CORE_PARAGRAPH => __DIR__ . '/resources/templates/InterimWordpressBlockParser/core-paragraph.twig',
+            self::BLOCK_CORE_IMAGE => __DIR__ . '/resources/templates/InterimWordpressBlockParser/core-image.twig',
+            self::BLOCK_CORE_LIST => __DIR__ . '/resources/templates/InterimWordpressBlockParser/core-list.twig',
+            self::BLOCK_CORE_LIST_ITEM => __DIR__ . '/resources/templates/InterimWordpressBlockParser/core-list-item.twig',
+            self::BLOCK_EO_POST => __DIR__ . '/resources/templates/InterimWordpressBlockParser/eo-post.twig',
+
+            default => throw new \Exception("not existing block template: '{$blockName}'"),
+        };
+
+        return file_get_contents($templateFile);
+    }
+}
