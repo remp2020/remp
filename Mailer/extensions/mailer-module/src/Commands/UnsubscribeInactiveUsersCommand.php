remp2020/remp

commit 7e8311d9c6a1226aeb110fdfbd5387270fcd7877

External mail_type and optional user_id in mail_user_subscriptions
remp/novydenik#1516

Mailer/extensions/mailer-module/src/Commands/UnsubscribeInactiveUsersCommand.php
@@ -114,7 +114,7 @@ class UnsubscribeInactiveUsersCommand extends Command
                         $this->userSubscriptionsRepository->unsubscribeUserFromAll($userId, $userEmail, $omitMailTypeCodes);
 
                         $eventOptions = new EventOptions();
-                        $eventOptions->setUser(new User(['id' => $userId]));
+                        $eventOptions->setUser(new User(id: $userId, email: $userEmail));
                         $this->tracker?->trackEvent(
                             new DateTime(),
                             'mail-type',

Mailer/extensions/mailer-module/src/Events/MailSentEventHandler.php
@@ -13,19 +13,18 @@ class MailSentEventHandler
 {
     use LoggerAwareTrait;
 
-    private $tracker;
-
-    public function __construct(ITracker $tracker)
+    public function __construct(private ITracker $tracker)
     {
-        $this->tracker = $tracker;
     }
 
     public function __invoke(MailSentEvent $event)
     {
         $options = new EventOptions();
-        $options->setUser(new User([
-            'id' => $event->getUserId(),
-        ]));
+        $options->setUser(new User(
+            id: $event->getUserId(),
+            email: $event->getEmail(),
+        ));
+
         $options->setFields([
             'email' => $event->getEmail(),
             'template_code' => $event->getTemplateCode(),

Mailer/extensions/mailer-module/src/Hermes/MailSentHandler.php
@@ -15,11 +15,8 @@ class MailSentHandler implements HandlerInterface
 {
     use LoggerAwareTrait;
 
-    private $tracker;
-
-    public function __construct(ITracker $tracker)
+    public function __construct(private ITracker $tracker)
     {
-        $this->tracker = $tracker;
     }
 
     public function handle(MessageInterface $message): bool
@@ -42,9 +39,10 @@ class MailSentHandler implements HandlerInterface
         }
 
         $options = new EventOptions();
-        $options->setUser(new User([
-            'id' => $payload['user_id'],
-        ]));
+        $options->setUser(new User(
+            id: $payload['user_id'],
+            email: $payload['user_email'] ?? null,
+        ));
         $options->setFields([
             'email' => $payload['email'],
             'template_code' => $payload['template_code'],

Mailer/extensions/mailer-module/src/Models/Tracker/User.php
@@ -3,30 +3,30 @@ declare(strict_types=1);
 
 namespace Remp\MailerModule\Models\Tracker;
 
-class User
+readonly class User
 {
-    private $id;
+    public function __construct(
+        private string|int|null $id = null,
+        private ?string $email = null,
+        private ?string $ipAddress = null,
+        private ?string $url = null,
+        private ?string $userAgent = null,
+    ) {
+    }
 
-    private $ip_address;
-
-    private $url;
-
-    private $user_agent;
-
-    public function __construct(array $options = [])
+    public function getId(): int|string|null
     {
-        foreach ($options as $key => $val) {
-            $this->{$key} = (string)$val;
-        }
+        return $this->id;
     }
 
     public function toArray(): array
     {
         return [
             'id' => $this->id,
-            'ip_address' => $this->ip_address,
+            'email' => $this->email,
+            'ip_address' => $this->ipAddress,
             'url' => $this->url,
-            'user_agent' => $this->user_agent,
+            'user_agent' => $this->userAgent,
         ];
     }
 }

Mailer/app/config/config.test.neon
@@ -12,6 +12,11 @@ parameters:
 		project_root: /data/web/remp2020.com/app/Mailer/current
 
 services:
+	nullLogger:
+		factory: Psr\Log\NullLogger
+	batchEmailGeneratorTestWrapper:
+		factory: Tests\Feature\Mails\BatchEmailGeneratorTestWrapper(@nullLogger)
+		autowired: false
 	- Remp\MailerModule\Models\Users\Dummy
 	hermesMemoryDriver:
 		factory: Remp\MailerModule\Hermes\MemoryDriver

Mailer/extensions/mailer-module/src/Api/v1/Handlers/Mailers/MailTypeUpsertHandler.php
@@ -16,22 +16,12 @@ use Tomaj\NetteApi\Response\ResponseInterface;
 
 class MailTypeUpsertHandler extends BaseHandler
 {
-    private $listsRepository;
-
-    private $listCategoriesRepository;
-
-    private $templatesRepository;
-
     public function __construct(
-        ListsRepository $listsRepository,
-        ListCategoriesRepository $listCategoriesRepository,
-        TemplatesRepository $templatesRepository
+        private ListsRepository $listsRepository,
+        private ListCategoriesRepository $listCategoriesRepository,
+        private TemplatesRepository $templatesRepository
     ) {
         parent::__construct();
-
-        $this->listsRepository = $listsRepository;
-        $this->listCategoriesRepository = $listCategoriesRepository;
-        $this->templatesRepository = $templatesRepository;
     }
 
     public function params(): array
@@ -157,6 +147,7 @@ class MailTypeUpsertHandler extends BaseHandler
                 'updated_at' => $list->updated_at->format(DATE_RFC3339),
                 'is_multi_variant' => (bool) $list->is_multi_variant,
                 'default_variant_id' => $list->default_variant_id,
+                'is_external' => (bool) $list->is_external,
                 'subscribe_mail_template_code' => $list->subscribe_mail_template->code ?? null,
                 'unsubscribe_mail_template_code' => $list->unsubscribe_mail_template->code ?? null,
             ],
@@ -214,20 +205,21 @@ class MailTypeUpsertHandler extends BaseHandler
 
 
         return $this->listsRepository->add(
-            $params['mail_type_category_id'],
-            $params['priority'],
-            $params['code'],
-            $params['title'],
-            $params['sorting'] ?? $this->getLastSortingNumberByCategory($params['mail_type_category_id']),
-            $params['auto_subscribe'] ?? false,
-            $params['locked'] ?? false,
-            $params['description'] ?? null,
-            $params['preview_url'] ?? null,
-            $params['page_url'] ?? null,
-            $params['image_url'] ?? null,
-            $params['public_listing'] ?? true,
-            $params['mail_from'] ?? null,
-            $params['subscribe_mail_template_id'] ?? null
+            categoryId: $params['mail_type_category_id'],
+            priority: $params['priority'],
+            code: $params['code'],
+            name: $params['title'],
+            sorting: $params['sorting'] ?? $this->getLastSortingNumberByCategory($params['mail_type_category_id']),
+            isAutoSubscribe: $params['auto_subscribe'] ?? false,
+            isLocked: $params['locked'] ?? false,
+            description: $params['description'] ?? null,
+            previewUrl: $params['preview_url'] ?? null,
+            pageUrl: $params['page_url'] ?? null,
+            imageUrl: $params['image_url'] ?? null,
+            publicListing: $params['public_listing'] ?? true,
+            mailFrom: $params['mail_from'] ?? null,
+            subscribeEmailTemplateId: $params['subscribe_mail_template_id'] ?? null,
+            isExternal: $params['is_external'] ?? false,
         );
     }
 }

Mailer/extensions/mailer-module/src/Api/v1/Handlers/Mailers/MailTypesListingHandler.php
@@ -63,6 +63,7 @@ class MailTypesListingHandler extends BaseHandler
             $item->is_multi_variant = (bool) $row->is_multi_variant;
             $item->sorting = $row->sorting;
             $item->variants = $this->listVariantsRepository->getVariantsForType($row)->order('sorting')->fetchPairs('id', 'title');
+            $item->is_external = (bool) $row->is_external;
             $output[] = $item;
         }
 

Mailer/extensions/mailer-module/src/Api/v1/Handlers/Users/BulkSubscribeHandler.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Remp\MailerModule\Api\v1\Handlers\Users;
 
+use Nette\Http\IResponse;
 use Nette\Utils\Strings;
 use Remp\MailerModule\Api\InvalidApiInputParamException;
 use Remp\MailerModule\Api\JsonValidationTrait;
@@ -43,9 +44,16 @@ class BulkSubscribeHandler extends SubscribeHandler
                 continue;
             }
 
+            if (!$list->is_external && !isset($item['user_id'])) {
+                $listIdentifier = $item['list_code'] ?? $item['list_id'];
+                $errorMessage = "Missing 'user_id', it's required when subscribing for mail_type [{$listIdentifier}]";
+                $errors = array_merge($errors, ["element_" . $iteration => $errorMessage]);
+                continue;
+            }
+
             $users[] = [
                 'email' => $item['email'],
-                'user_id' => $item['user_id'],
+                'user_id' => $item['user_id'] ?? null,
                 'list' => $list,
                 'variant_id' => $variantID,
                 'subscribe' => $item['subscribe'],
@@ -55,8 +63,16 @@ class BulkSubscribeHandler extends SubscribeHandler
             ];
         }
 
+        if (!empty($errors)) {
+            return new JsonApiResponse(400, [
+                'status' => 'error',
+                'message' => 'Input data contains errors. See included list of errors.',
+                'errors' => $errors,
+            ]);
+        }
+
         foreach ($users as $user) {
-            $rtmParams = $item['rtm_params'] ?? [];
+            $rtmParams = $user['rtm_params'] ?? [];
 
             if ($user['subscribe'] === true) {
                 $this->userSubscriptionsRepository->subscribeUser(

commit 8c742a1036822236c6c04831d65b5d3438db5a76

Mailer - do not track email to tracker
This is a bug introduced in commit 7e8311d9c6a1226aeb110fdfbd5387270fcd7877

remp/novydenik#1516

Mailer/extensions/mailer-module/src/Commands/UnsubscribeInactiveUsersCommand.php
@@ -114,7 +114,7 @@ class UnsubscribeInactiveUsersCommand extends Command
                         $this->userSubscriptionsRepository->unsubscribeUserFromAll($userId, $userEmail, $omitMailTypeCodes);
 
                         $eventOptions = new EventOptions();
-                        $eventOptions->setUser(new User(id: $userId, email: $userEmail));
+                        $eventOptions->setUser(new User(id: $userId));
                         $this->tracker?->trackEvent(
                             new DateTime(),
                             'mail-type',

Mailer/extensions/mailer-module/src/Events/MailSentEventHandler.php
@@ -22,7 +22,6 @@ class MailSentEventHandler
         $options = new EventOptions();
         $options->setUser(new User(
             id: $event->getUserId(),
-            email: $event->getEmail(),
         ));
 
         $options->setFields([

Mailer/extensions/mailer-module/src/Hermes/MailSentHandler.php
@@ -41,7 +41,6 @@ class MailSentHandler implements HandlerInterface
         $options = new EventOptions();
         $options->setUser(new User(
             id: $payload['user_id'],
-            email: $payload['user_email'] ?? null,
         ));
         $options->setFields([
             'email' => $payload['email'],

Mailer/extensions/mailer-module/src/Models/Tracker/User.php
@@ -7,7 +7,6 @@ readonly class User
 {
     public function __construct(
         private string|int|null $id = null,
-        private ?string $email = null,
         private ?string $ipAddress = null,
         private ?string $url = null,
         private ?string $userAgent = null,
@@ -23,7 +22,6 @@ readonly class User
     {
         return [
             'id' => $this->id,
-            'email' => $this->email,
             'ip_address' => $this->ipAddress,
             'url' => $this->url,
             'user_agent' => $this->userAgent,